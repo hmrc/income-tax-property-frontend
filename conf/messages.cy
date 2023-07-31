26ee0bfc6571919d1728570ed092c5a53b40dad6
Roberto Tena Garcia <roberto.tena.garcia@digital.hmrc.gov.uk>
2023-07-31 13:45:16 +0100

    SASS-5243 add the summary page

---
+++ conf/messages.cy
@@ -0,0 +1,6 @@
+service.name = Update and submit an Income Tax Return
+
+common.about = About
+common.notStarted = Not started
+
+summary.heading = About your UK Properties
---
+++ test/controllers/SummaryControllerSpec.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import views.html.{IndexView, SummaryView}
+
+import java.time.LocalDate
+
+class SummaryControllerSpec extends SpecBase {
+
+  "Summary Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val year = LocalDate.now().getYear
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.SummaryController.show(year).url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[SummaryView]
+
+        status(result) mustEqual OK
+
+        contentAsString(result) mustEqual view()(request, messages(application)).toString
+      }
+    }
+  }
+}
