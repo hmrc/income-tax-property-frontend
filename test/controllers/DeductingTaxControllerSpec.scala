b7952076f17aa2069b432584bd8ece46b3b56837
vivekhmrc <vivek.kumar@digital.hmrc.gov.uk>
2023-09-08 14:46:23 +0100

    Update DeductingTaxControllerSpec.scala

--- test/controllers/DeductingTaxControllerSpec.scala
+++ test/controllers/DeductingTaxControllerSpec.scala
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.SpecBase
-import forms.DeductingTaxFormProvider
+import forms.{DeductingTax, DeductingTaxFormProvider}
 import models.{NormalMode, UserAnswers}
 import navigation.{FakeNavigator, Navigator}
 import org.mockito.ArgumentMatchers.any
@@ -58,7 +74,7 @@ class DeductingTaxControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(true), NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form.fill(DeductingTax(true, None)), NormalMode)(request, messages(application)).toString
       }
     }
 
