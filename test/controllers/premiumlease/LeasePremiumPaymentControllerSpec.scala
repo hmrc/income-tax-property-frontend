7afe375319bbacf577c50a5c2846320ffe6a7f8c
adam-shahzad-2 <adamshahzad343@gmail.com>
2023-09-08 11:30:55 +0100

    removed duplicate work

--- test/controllers/premiumlease/LeasePremiumPaymentControllerSpec.scala
+++ test/controllers/premiumlease/LeasePremiumPaymentControllerSpec.scala
@@ -30,7 +30,7 @@ import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import repositories.SessionRepository
-import import views.html.premiumlease.LeasePremiumPaymentView
+import views.html.premiumlease.LeasePremiumPaymentView
 import controllers.premiumlease.routes._
 
 import java.time.LocalDate
