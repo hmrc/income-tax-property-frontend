91cdc9a20fdfa9a420caa79e3d63adb4e9f48e2b
adam-shahzad-2 <adamshahzad343@gmail.com>
2023-09-08 10:31:48 +0100

    fix test compile again

--- test/controllers/premiumLease/CalculatedFigureYourselfControllerSpec.scala
+++ test/controllers/premiumLease/CalculatedFigureYourselfControllerSpec.scala
@@ -136,7 +136,7 @@ class CalculatedFigureYourselfControllerSpec extends SpecBase with MockitoSugar 
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
       }
     }
 
@@ -152,7 +152,7 @@ class CalculatedFigureYourselfControllerSpec extends SpecBase with MockitoSugar 
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
       }
     }
   }
