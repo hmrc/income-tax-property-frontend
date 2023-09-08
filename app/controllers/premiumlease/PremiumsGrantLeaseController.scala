34279b94d8772beab05a510077fe5be725ba9ba0
adam-shahzad-2 <adamshahzad343@gmail.com>
2023-09-08 12:57:39 +0100

    filled in values

--- app/controllers/premiumlease/PremiumsGrantLeaseController.scala
+++ app/controllers/premiumlease/PremiumsGrantLeaseController.scala
@@ -31,42 +31,55 @@ import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 class PremiumsGrantLeaseController @Inject()(
-                                        override val messagesApi: MessagesApi,
-                                        sessionRepository: SessionRepository,
-                                        navigator: Navigator,
-                                        identify: IdentifierAction,
-                                        getData: DataRetrievalAction,
-                                        requireData: DataRequiredAction,
-                                        formProvider: PremiumsGrantLeaseFormProvider,
-                                        val controllerComponents: MessagesControllerComponents,
-                                        view: PremiumsGrantLeaseView
-                                      )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+                                              override val messagesApi: MessagesApi,
+                                              sessionRepository: SessionRepository,
+                                              navigator: Navigator,
+                                              identify: IdentifierAction,
+                                              getData: DataRetrievalAction,
+                                              requireData: DataRequiredAction,
+                                              formProvider: PremiumsGrantLeaseFormProvider,
+                                              val controllerComponents: MessagesControllerComponents,
+                                              view: PremiumsGrantLeaseView
+                                            )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
   val form = formProvider()
 
   def onPageLoad(taxYear: Int, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
+      val receivedGrantLeaseAmount: Option[Int] = request.userAnswers.get(RecievedGrantLeaseAmountPage)
+      val totalYearPeriods: Option[Int] = request.userAnswers.get(YearLeaseAmountPage)
 
-      val preparedForm = request.userAnswers.get(PremiumsGrantLeasePage) match {
-        case None => form
-        case Some(value) => form.fill(value)
+      (receivedGrantLeaseAmount, totalYearPeriods) match {
+        case (None, _) => Redirect(routes.RecievedGrantLeaseAmountController.onPageLoad(taxYear, mode))
+        case (_, None) => Redirect(routes.RecievedGrantLeaseAmountController.onPageLoad(taxYear, mode))
+        case (amount, period) =>
+          val preparedForm = request.userAnswers.get(PremiumsGrantLeasePage) match {
+            case None => form
+            case Some(value) => form.fill(value)
+          }
+
+          Ok(view(preparedForm, taxYear, amount.get, period.get, mode))
       }
-
-      Ok(view(preparedForm, taxYear, mode))
   }
 
   def onSubmit(taxYear: Int, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
+      val receivedGrantLeaseAmount: Option[Int] = request.userAnswers.get(RecievedGrantLeaseAmountPage)
+      val totalYearPeriods: Option[Int] = request.userAnswers.get(YearLeaseAmountPage)
 
-      form.bindFromRequest().fold(
-        formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors,taxYear, mode))),
+      (receivedGrantLeaseAmount, totalYearPeriods) match {
+        case (None, _) => Future.successful(Redirect(routes.RecievedGrantLeaseAmountController.onPageLoad(taxYear, mode)))
+        case (_, None) => Future.successful(Redirect(routes.RecievedGrantLeaseAmountController.onPageLoad(taxYear, mode)))
+        case (amount, period) =>
+          form.bindFromRequest().fold(
+            formWithErrors =>
+              Future.successful(BadRequest(view(formWithErrors, taxYear, amount.get, period.get, mode))),
 
-        value =>
-          for {
-            updatedAnswers <- Future.fromTry(request.userAnswers.set(PremiumsGrantLeasePage, value))
-            _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(PremiumsGrantLeasePage, taxYear ,mode, updatedAnswers))
-      )
+            value =>
+              for {
+                updatedAnswers <- Future.fromTry(request.userAnswers.set(PremiumsGrantLeasePage, value))
+                _ <- sessionRepository.set(updatedAnswers)
+              } yield Redirect(navigator.nextPage(PremiumsGrantLeasePage, taxYear, mode, updatedAnswers))
+          )
+      }
   }
-}
