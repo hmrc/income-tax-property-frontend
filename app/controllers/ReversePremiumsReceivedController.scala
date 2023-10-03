7b48e2588129b1de7d80a80e08a42b02eacc9820
adam-shahzad-2 <adamshahzad343@gmail.com>
2023-10-03 09:08:03 +0100

    added taxyear

--- app/controllers/ReversePremiumsReceivedController.scala
+++ app/controllers/ReversePremiumsReceivedController.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import controllers.actions._
@@ -28,7 +44,7 @@ class ReversePremiumsReceivedController @Inject()(
 
   val form = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad(taxYear: Int, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
 
       val preparedForm = request.userAnswers.get(ReversePremiumsReceivedPage) match {
@@ -36,21 +52,21 @@ class ReversePremiumsReceivedController @Inject()(
         case Some(value) => form.fill(value)
       }
 
-      Ok(view(preparedForm, mode))
+      Ok(view(preparedForm, mode, taxYear))
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+  def onSubmit(taxYear: Int,mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
 
       form.bindFromRequest().fold(
         formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors, mode))),
+          Future.successful(BadRequest(view(formWithErrors, mode, taxYear))),
 
         value =>
           for {
             updatedAnswers <- Future.fromTry(request.userAnswers.set(ReversePremiumsReceivedPage, value))
             _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(ReversePremiumsReceivedPage, mode, updatedAnswers))
+          } yield Redirect(navigator.nextPage(ReversePremiumsReceivedPage, taxYear, mode, updatedAnswers))
       )
   }
 }
