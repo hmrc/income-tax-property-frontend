ad6ec53e203d4895190afe869527435e460f5de9
adam-shahzad-2 <adamshahzad343@gmail.com>
2023-10-11 12:06:02 +0100

    fixes

--- test/pages/propertyrentals/CalculatedFigureYoureselfPageSpec.scala
+++ test/pages/propertyrentals/CalculatedFigureYoureselfPageSpec.scala
@@ -10,9 +10,9 @@ class CalculatedFigureYoureselfPageSpec extends SpecBase {
   "must remove the correct data when the answer is yes" in {
 
     val userData = emptyUserAnswers
-                      .set(RecievedGrantLeaseAmountPage, BigDecimal(10.11)).get
-                      .set(YearLeaseAmountPage, 10).get
-                      .set(PremiumsGrantLeasePage, BigDecimal(10.12)).get
+                      .set(RecievedGrantLeaseAmountPage, BigDecimal(10.11)).success.value
+                      .set(YearLeaseAmountPage, 10).success.value
+                      .set(PremiumsGrantLeasePage, BigDecimal(10.12)).success.value
 
     val result = userData.set(CalculatedFigureYourselfPage, CalculatedFigureYourself(true, Some(10.13))).success.value
 
