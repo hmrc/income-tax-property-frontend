c8c01f4c290f3ea8081324eaa0c7482fb87b0081
vivekhmrc <vivek.kumar@digital.hmrc.gov.uk>
2023-10-19 09:11:43 +0100

    SASS-5681: Added Private Use Adjustment

---
+++ app/controllers/PrivateUseAdjustmentController.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions._
+import forms.PrivateUseAdjustmentFormProvider
+import models.Mode
+import navigation.Navigator
+import pages.PrivateUseAdjustmentPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.PrivateUseAdjustmentView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class PrivateUseAdjustmentController @Inject()(
+                                         override val messagesApi: MessagesApi,
+                                         sessionRepository: SessionRepository,
+                                         navigator: Navigator,
+                                         identify: IdentifierAction,
+                                         getData: DataRetrievalAction,
+                                         requireData: DataRequiredAction,
+                                         formProvider: PrivateUseAdjustmentFormProvider,
+                                         val controllerComponents: MessagesControllerComponents,
+                                         view: PrivateUseAdjustmentView
+                                       )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad(taxYear: Int, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+      val form = formProvider(request.isAgentMessageKey)
+      val preparedForm = request.userAnswers.get(PrivateUseAdjustmentPage) match {
+        case None => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, taxYear, mode, request.isAgentMessageKey))
+  }
+
+  def onSubmit(taxYear: Int, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+    implicit request =>
+      val form = formProvider(request.isAgentMessageKey)
+      form.bindFromRequest().fold(
+        formWithErrors =>
+          Future.successful(BadRequest(view(formWithErrors, taxYear, mode, request.isAgentMessageKey))),
+
+        value =>
+          for {
+            updatedAnswers <- Future.fromTry(request.userAnswers.set(PrivateUseAdjustmentPage, value))
+            _ <- sessionRepository.set(updatedAnswers)
+          } yield Redirect(navigator.nextPage(PrivateUseAdjustmentPage, taxYear, mode, updatedAnswers))
+      )
+  }
+}
