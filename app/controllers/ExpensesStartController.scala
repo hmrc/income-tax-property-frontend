5048decb95dfca1e4f7289e1eb1a7fb84270e3c5
Arun Kumar Madesh <56151567+arunkumar461@users.noreply.github.com>
2023-11-20 14:32:41 +0000

    Add expenses start page

---
+++ app/controllers/ExpensesStartController.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions._
+import models.requests.DataRequest
+import pages.premiumlease.PremiumsGrantLeasePage
+import pages.{CalculatedFigureYourselfPage, IncomeFromPropertyRentalsPage, OtherIncomeFromPropertyPage, ReversePremiumsReceivedPage}
+
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.ExpensesStartOver85KIncomeView
+
+class ExpensesStartController @Inject()(
+                                                      override val messagesApi: MessagesApi,
+                                                      identify: IdentifierAction,
+                                                      getData: DataRetrievalAction,
+                                                      requireData: DataRequiredAction,
+                                                      val controllerComponents: MessagesControllerComponents,
+                                                      view: ExpensesStartOver85KIncomeView
+                                                    ) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad(taxYear: Int): Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+      val totalIncomeCapped = 85000
+      val isUnder85K = totalIncome(request) < BigDecimal(totalIncomeCapped)
+      Ok(view(taxYear, isUnder85K))
+  }
+
+  private def totalIncome(request: DataRequest[AnyContent]): BigDecimal = {
+    val incomeFromPropertyRentals = request.userAnswers.get(IncomeFromPropertyRentalsPage).getOrElse(BigDecimal(0))
+    val leasePremiumCalculated = request.userAnswers.get(CalculatedFigureYourselfPage).flatMap(cf => cf.amount).getOrElse(BigDecimal(0))
+    val reversePremiumsReceived = request.userAnswers.get(ReversePremiumsReceivedPage).flatMap(cf => cf.amount).getOrElse(BigDecimal(0))
+    val premiumsGrantLease = request.userAnswers.get(PremiumsGrantLeasePage).getOrElse(BigDecimal(0))
+    val otherIncome = request.userAnswers.get(OtherIncomeFromPropertyPage).map(i => i.amount).getOrElse(BigDecimal(0))
+
+    incomeFromPropertyRentals + leasePremiumCalculated + premiumsGrantLease + reversePremiumsReceived + otherIncome
+
+  }
+}
---
+++ test/controllers/ExpensesStartControllerSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import views.html.ExpensesStartOver85KIncomeView
+
+class ExpensesStartControllerSpec extends SpecBase {
+
+  "ExpensesStartOver85KIncome Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+      val taxYear = 2023
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = true).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.ExpensesStartController.onPageLoad(taxYear).url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[ExpensesStartOver85KIncomeView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(taxYear, isUnder85K = true)(request, messages(application)).toString
+      }
+    }
+  }
+}
