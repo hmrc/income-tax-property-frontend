7adaf78f8334a7ea425ca0161c24654368210511
willjshark <90686675+willjshark@users.noreply.github.com>
2023-11-22 16:57:54 +0000

    Write local tests for ConsolidatedExpenses page

--- test/controllers/ConsolidatedExpensesControllerSpec.scala
+++ test/controllers/ConsolidatedExpensesControllerSpec.scala
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.SpecBase
 import forms.ConsolidatedExpensesFormProvider
-import models.{NormalMode, UserAnswers}
+import models.{NormalMode, ConsolidatedExpenses, UserAnswers}
 import navigation.{FakeNavigator, Navigator}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
@@ -15,127 +31,130 @@ import play.api.test.Helpers._
 import repositories.SessionRepository
 import views.html.ConsolidatedExpensesView
 
+import java.time.LocalDate
 import scala.concurrent.Future
 
-class ConsolidatedExpensesControllerSpec extends SpecBase with MockitoSugar {
 
-  def onwardRoute = Call("GET", "/foo")
+  class ConsolidatedExpensesControllerSpec extends SpecBase with MockitoSugar {
 
-  val formProvider = new ConsolidatedExpensesFormProvider()
-  val form = formProvider()
+    def onwardRoute = Call("GET", "/")
+    private val taxYear = LocalDate.now.getYear
 
-  lazy val consolidatedExpensesRoute = routes.ConsolidatedExpensesController.onPageLoad(NormalMode).url
+    val formProvider = new ConsolidatedExpensesFormProvider()
+    val form = formProvider("individual")
 
-  "ConsolidatedExpenses Controller" - {
+    lazy val consolidatedExpensesRoute = routes.ConsolidatedExpensesController.onPageLoad(taxYear, NormalMode).url
 
-    "must return OK and the correct view for a GET" in {
+    "ConsolidatedExpenses Controller" - {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      "must return OK and the correct view for a GET" in {
 
-      running(application) {
-        val request = FakeRequest(GET, consolidatedExpensesRoute)
+        val application = applicationBuilder( userAnswers = Some(emptyUserAnswers), false).build()
 
-        val result = route(application, request).value
+        running(application) {
+          val request = FakeRequest(GET, consolidatedExpensesRoute)
 
-        val view = application.injector.instanceOf[ConsolidatedExpensesView]
+          val result = route(application, request).value
 
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+          val view = application.injector.instanceOf[ConsolidatedExpensesView]
+
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual view(form, NormalMode, taxYear, "individual")(request, messages(application)).toString
+        }
       }
-    }
 
-    "must populate the view correctly on a GET when the question has previously been answered" in {
+      "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val userAnswers = UserAnswers(userAnswersId).set(ConsolidatedExpensesPage, true).success.value
+        val userAnswers = UserAnswers(userAnswersId).set(ConsolidatedExpensesPage, ConsolidatedExpenses(true, Some(12.34))).success.value
 
-      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+        val application = applicationBuilder(userAnswers = Some(userAnswers), false).build()
 
-      running(application) {
-        val request = FakeRequest(GET, consolidatedExpensesRoute)
+        running(application) {
+          val request = FakeRequest(GET, consolidatedExpensesRoute)
 
-        val view = application.injector.instanceOf[ConsolidatedExpensesView]
+          val view = application.injector.instanceOf[ConsolidatedExpensesView]
 
-        val result = route(application, request).value
+          val result = route(application, request).value
 
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(true), NormalMode)(request, messages(application)).toString
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual view(form.fill(ConsolidatedExpenses(true, Some(12.34))), NormalMode, taxYear, "individual")(request, messages(application)).toString
+        }
       }
-    }
 
-    "must redirect to the next page when valid data is submitted" in {
+      "must redirect to the next page when valid data is submitted" in {
 
-      val mockSessionRepository = mock[SessionRepository]
+        val mockSessionRepository = mock[SessionRepository]
 
-      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+        when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
 
-      val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers))
-          .overrides(
-            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
-            bind[SessionRepository].toInstance(mockSessionRepository)
-          )
-          .build()
+        val application =
+          applicationBuilder(userAnswers = Some(emptyUserAnswers), false)
+            .overrides(
+              bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+              bind[SessionRepository].toInstance(mockSessionRepository)
+            )
+            .build()
 
-      running(application) {
-        val request =
-          FakeRequest(POST, consolidatedExpensesRoute)
-            .withFormUrlEncodedBody(("value", "true"))
+        running(application) {
+          val request =
+            FakeRequest(POST, consolidatedExpensesRoute)
+              .withFormUrlEncodedBody("consolidatedExpenses" -> "true", "consolidatedExpensesAmount" -> "1234")
 
-        val result = route(application, request).value
+          val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual onwardRoute.url
+          status(result) mustEqual SEE_OTHER
+          redirectLocation(result).value mustEqual onwardRoute.url
+        }
       }
-    }
 
-    "must return a Bad Request and errors when invalid data is submitted" in {
+      "must return a Bad Request and errors when invalid data is submitted" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), false).build()
 
-      running(application) {
-        val request =
-          FakeRequest(POST, consolidatedExpensesRoute)
-            .withFormUrlEncodedBody(("value", ""))
+        running(application) {
+          val request =
+            FakeRequest(POST, consolidatedExpensesRoute)
+              .withFormUrlEncodedBody(("value", ""))
 
-        val boundForm = form.bind(Map("value" -> ""))
+          val boundForm = form.bind(Map("value" -> ""))
 
-        val view = application.injector.instanceOf[ConsolidatedExpensesView]
+          val view = application.injector.instanceOf[ConsolidatedExpensesView]
 
-        val result = route(application, request).value
+          val result = route(application, request).value
 
-        status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+          status(result) mustEqual BAD_REQUEST
+          contentAsString(result) mustEqual view(boundForm, NormalMode, taxYear, "individual")(request, messages(application)).toString
+        }
       }
-    }
 
-    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+      "must redirect to Journey Recovery for a GET if no existing data is found" in {
 
-      val application = applicationBuilder(userAnswers = None, true).build()
+        val application = applicationBuilder(userAnswers = None, true).build()
 
-      running(application) {
-        val request = FakeRequest(GET, consolidatedExpensesRoute)
+        running(application) {
+          val request = FakeRequest(GET, consolidatedExpensesRoute)
 
-        val result = route(application, request).value
+          val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+          status(result) mustEqual SEE_OTHER
+          redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        }
       }
-    }
 
-    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+      "must redirect to Journey Recovery for a POST if no existing data is found" in {
 
-      val application = applicationBuilder(userAnswers = None, true).build()
+        val application = applicationBuilder(userAnswers = None, true).build()
 
-      running(application) {
-        val request =
-          FakeRequest(POST, consolidatedExpensesRoute)
-            .withFormUrlEncodedBody(("value", "true"))
+        running(application) {
+          val request =
+            FakeRequest(POST, consolidatedExpensesRoute)
+              .withFormUrlEncodedBody(("value", "true"))
 
-        val result = route(application, request).value
+          val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+          status(result) mustEqual SEE_OTHER
+          redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        }
       }
     }
   }
-}
