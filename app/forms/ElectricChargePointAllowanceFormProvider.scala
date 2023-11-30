a44545231700fb26ffe5673b40e14a6f13a9f436
Arun Kumar Madesh <56151567+arunkumar461@users.noreply.github.com>
2023-11-30 18:27:15 +0000

    Add Electric charge-point allowance

---
+++ app/forms/ElectricChargePointAllowanceFormProvider.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.mappings.Mappings
+import models.ElectricChargePointAllowance
+
+import javax.inject.Inject
+import play.api.data.Form
+import play.api.data.Forms.mapping
+import uk.gov.voa.play.form.ConditionalMappings.mandatoryIfTrue
+
+class ElectricChargePointAllowanceFormProvider @Inject() extends Mappings {
+  val minimum = 0;
+  val maximum = 100000000;
+
+  def apply(individualOrAgent: String): Form[ElectricChargePointAllowance] =
+    Form[ElectricChargePointAllowance](
+      mapping(
+        "yesOrNo" -> boolean(s"electricChargePointAllowance.error.required.$individualOrAgent"),
+        "allowance" -> {
+          mandatoryIfTrue("yesOrNo",
+            currency(
+              s"electricChargePointAllowance.amount.error.required.$individualOrAgent",
+              s"electricChargePointAllowance.amount.error.twoDecimalPlaces.$individualOrAgent",
+              s"electricChargePointAllowance.amount.error.nonNumeric.$individualOrAgent")
+              .verifying(inRange(BigDecimal(minimum), BigDecimal(maximum),
+                s"electricChargePointAllowance.amount.error.outOfRange"))
+          )
+        }
+      )(ElectricChargePointAllowance.apply)(ElectricChargePointAllowance.unapply)
+    )
+}
