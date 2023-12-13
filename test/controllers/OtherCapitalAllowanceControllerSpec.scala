1c0d38452207ede02a4803cbfd3dfb96e076c21b
vivekhmrc <vivek.kumar@digital.hmrc.gov.uk>
2023-12-13 06:53:37 -0500

    SASS-6143: Other capital allowances view (#84)
    * SASS-6143: Other capital allowances view

---
+++ test/controllers/OtherCapitalAllowanceControllerSpec.scala
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import forms.OtherCapitalAllowanceFormProvider
+import models.{NormalMode, UserAnswers}
+import navigation.{FakeNavigator, Navigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.OtherCapitalAllowancePage
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.api.data.Form
+import repositories.SessionRepository
+import views.html.allowances.OtherCapitalAllowanceView
+
+import scala.concurrent.Future
+
+class OtherCapitalAllowanceControllerSpec extends SpecBase with MockitoSugar {
+
+  val formProvider = new OtherCapitalAllowanceFormProvider()
+  private val isAgentMessageKey = "individual"
+  val form: Form[BigDecimal] = formProvider(isAgentMessageKey)
+
+  def onwardRoute = Call("GET", "/foo")
+
+  val validAnswer: BigDecimal = BigDecimal(0)
+  val taxYear = 2023
+
+  lazy val otherCapitalAllowanceRoute = routes.OtherCapitalAllowanceController.onPageLoad(taxYear, NormalMode).url
+
+  "OtherCapitalAllowance Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false).build()
+
+      running(application) {
+        val request = FakeRequest(GET, otherCapitalAllowanceRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[OtherCapitalAllowanceView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, taxYear, isAgentMessageKey, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = UserAnswers(userAnswersId).set(OtherCapitalAllowancePage, validAnswer).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers), isAgent = false).build()
+
+      running(application) {
+        val request = FakeRequest(GET, otherCapitalAllowanceRoute)
+
+        val view = application.injector.instanceOf[OtherCapitalAllowanceView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form.fill(validAnswer), taxYear, isAgentMessageKey, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false)
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, otherCapitalAllowanceRoute)
+            .withFormUrlEncodedBody(("otherCapitalAllowance", validAnswer.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, otherCapitalAllowanceRoute)
+            .withFormUrlEncodedBody(("otherCapitalAllowance", "invalid value"))
+
+        val boundForm = form.bind(Map("otherCapitalAllowance" -> "invalid value"))
+
+        val view = application.injector.instanceOf[OtherCapitalAllowanceView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, taxYear, isAgentMessageKey, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None, isAgent = false).build()
+
+      running(application) {
+        val request = FakeRequest(GET, otherCapitalAllowanceRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None, isAgent = false).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, otherCapitalAllowanceRoute)
+            .withFormUrlEncodedBody(("otherCapitalAllowance", validAnswer.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
