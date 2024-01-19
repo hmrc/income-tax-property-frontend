68154f8bc8f9aae23858191e740bc5e93b926770
Arun Kumar Madesh <56151567+arunkumar461@users.noreply.github.com>
2024-01-19 16:20:55 +0000

    SASS-6415: 18. NON-PIA SBA: Claim Amount Page (#95)
    * Add structures and buildings allowance claim amount page

---
+++ test/controllers/StructureBuildingAllowanceClaimControllerSpec.scala
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import forms.StructureBuildingAllowanceClaimFormProvider
+import models.{NormalMode, UserAnswers}
+import navigation.{FakeNavigator, Navigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.StructureBuildingAllowanceClaimPage
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.api.data.Form
+import repositories.SessionRepository
+import views.html.StructureBuildingAllowanceClaimView
+
+import scala.concurrent.Future
+
+class StructureBuildingAllowanceClaimControllerSpec extends SpecBase with MockitoSugar {
+
+  val formProvider = new StructureBuildingAllowanceClaimFormProvider()
+  private val isAgentMessageKey = "individual"
+  val form: Form[BigDecimal] = formProvider(isAgentMessageKey)
+
+  def onwardRoute: Call = Call("GET", "/foo")
+
+  val validAnswer: BigDecimal = BigDecimal(0)
+  val taxYear = 2023
+
+  lazy val structureBuildingAllowanceClaimRoute: String = routes.StructureBuildingAllowanceClaimController.onPageLoad(taxYear, NormalMode).url
+
+  "StructureBuildingAllowanceClaim Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false).build()
+
+      running(application) {
+        val request = FakeRequest(GET, structureBuildingAllowanceClaimRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[StructureBuildingAllowanceClaimView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, taxYear, isAgentMessageKey, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = UserAnswers(userAnswersId).set(StructureBuildingAllowanceClaimPage, validAnswer).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers), isAgent = false).build()
+
+      running(application) {
+        val request = FakeRequest(GET, structureBuildingAllowanceClaimRoute)
+
+        val view = application.injector.instanceOf[StructureBuildingAllowanceClaimView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form.fill(validAnswer), taxYear, isAgentMessageKey, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false)
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, structureBuildingAllowanceClaimRoute)
+            .withFormUrlEncodedBody(("structureBuildingAllowanceClaim", validAnswer.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, structureBuildingAllowanceClaimRoute)
+            .withFormUrlEncodedBody(("structureBuildingAllowanceClaim", "invalid value"))
+
+        val boundForm = form.bind(Map("structureBuildingAllowanceClaim" -> "invalid value"))
+
+        val view = application.injector.instanceOf[StructureBuildingAllowanceClaimView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, taxYear, isAgentMessageKey, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None, isAgent = false).build()
+
+      running(application) {
+        val request = FakeRequest(GET, structureBuildingAllowanceClaimRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None, isAgent = false).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, structureBuildingAllowanceClaimRoute)
+            .withFormUrlEncodedBody(("structureBuildingAllowanceClaim", validAnswer.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
