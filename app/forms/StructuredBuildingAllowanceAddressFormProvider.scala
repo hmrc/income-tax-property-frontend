6cea2cc3453dbcaeaf012430d882638f836170ca
adam-shahzad-2 <adamshahzad343@gmail.com>
2024-01-29 12:16:42 +0000

    Add SBA Address

---
+++ app/forms/StructuredBuildingAllowanceAddressFormProvider.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.mappings.Mappings
+import models.StructuredBuildingAllowanceAddress
+import play.api.data.Form
+import play.api.data.Forms.mapping
+import play.api.data.validation.Constraints.pattern
+
+import javax.inject.Inject
+import scala.util.matching.Regex
+
+class StructuredBuildingAllowanceAddressFormProvider @Inject() extends Mappings {
+  val postcodeRegex: Regex = "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\\s?[0-9][A-Za-z]{2})$".r
+
+  def apply: Form[StructuredBuildingAllowanceAddress] =
+    Form(mapping(
+      "buildingName" -> text("structureBuildingAllowanceAddress.buildingName.error.required")
+        .verifying(maxLength(90, "structureBuildingAllowanceAddress.buildingName.error.max")),
+      "buildingNumber" -> text("structureBuildingAllowanceAddress.buildingNumber.error.required"),
+      "postcode" -> text("structureBuildingAllowanceAddress.postcode.error.required").verifying(
+        pattern(postcodeRegex, "PostCode", "structureBuildingAllowanceAddress.postcode.error.invalid")))
+      (StructuredBuildingAllowanceAddress.apply)(StructuredBuildingAllowanceAddress.unapply
+    ))
+}
