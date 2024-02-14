69cad734834013067e6116ed93ec2dd7c11732d8
Arun Kumar Madesh <56151567+arunkumar461@users.noreply.github.com>
2024-02-14 17:45:21 +0000

    Upgrade play-frontend-hmrc-play (#116)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % bootstrapVersion,
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "8.5.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "2.0.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % hmrcMongoVersion,
