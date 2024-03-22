71f95baf80f49be6d002e8ac1de91e24e7150b3f
Arun Kumar Madesh <56151567+arunkumar461@users.noreply.github.com>
2024-03-22 17:23:48 +0000

    Bump up bootstrapVersion to 8.5.0 (#136)
    * update bootstrap version and sbt plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.4.0"
+  private val bootstrapVersion = "8.5.0"
   private val hmrcMongoVersion = "1.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "8.5.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "2.0.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % hmrcMongoVersion,
