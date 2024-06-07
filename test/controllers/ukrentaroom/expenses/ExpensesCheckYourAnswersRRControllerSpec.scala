ffca6e7c73bf286e94a895811aa83bef97c6c700
Costas Mantzoukas <159533843+cmantzoukashmrc@users.noreply.github.com>
2024-06-07 21:07:47 +0100

    SASS-8129 Fix error in tests

--- test/controllers/ukrentaroom/expenses/ExpensesCheckYourAnswersRRControllerSpec.scala
+++ test/controllers/ukrentaroom/expenses/ExpensesCheckYourAnswersRRControllerSpec.scala
@@ -16,17 +16,35 @@
 
 package controllers.ukrentaroom.expenses
 
+import audit.RentARoomExpenses
 import base.SpecBase
 import controllers.ukrentaroom.expenses.routes._
+import models.{JourneyContext, UserAnswers}
+import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar.mock
+import pages.PageConstants
+import play.api.inject.bind
+import play.api.libs.json.{JsNumber, Json}
 import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import service.PropertySubmissionService
 import viewmodels.govuk.SummaryListFluency
 import views.html.ukrentaroom.expenses.ExpensesCheckYourAnswersRRView
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 class ExpensesCheckYourAnswersRRControllerSpec extends SpecBase with SummaryListFluency {
 
-  def onwardRoute: Call = Call("GET", "/update-and-submit-income-tax-return/property/2023/summary")
+  val taxYear = 2024
+  private val propertySubmissionService = mock[PropertySubmissionService]
+  val context =
+    JourneyContext(taxYear = taxYear, mtditid = "mtditid", nino = "nino", journeyName = "rent-a-room-expenses")
+
+  def onwardRoute: Call = Call("GET", "/update-and-submit-income-tax-return/property/2024/summary")
 
   "ExpensesCheckYourRRAnswers Controller" - {
 
@@ -34,7 +52,7 @@ class ExpensesCheckYourAnswersRRControllerSpec extends SpecBase with SummaryList
 
       val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = true).build()
       val list = SummaryListViewModel(Seq.empty)
-      val taxYear = 2023
+      val taxYear = 2024
       running(application) {
 
         val request = FakeRequest(GET, ExpensesCheckYourAnswersRRController.onPageLoad(taxYear).url)
@@ -62,8 +80,54 @@ class ExpensesCheckYourAnswersRRControllerSpec extends SpecBase with SummaryList
     }
 
     "must return OK and the correct view for a POST (onSubmit)" in {
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = true).build()
-      val taxYear = 2023
+
+      val rentsRatesAndInsurance = 100
+      val repairsAndMaintenanceCosts = 200
+      val loanInterest = 300
+      val otherProfessionalFee = 400
+      val costsOfServicesProvided = 500
+      val propertyBusinessTravelCost = 600
+      val otherAllowablePropertyExpenses = 700
+
+      val answers = UserAnswers(
+        id = "rent-a-room-expenses-userId",
+        data = Json.obj(
+          PageConstants.rentARoomExpense -> Json.obj(
+            "rentsRatesAndInsurance"           -> JsNumber(rentsRatesAndInsurance),
+            "repairsAndMaintenanceCosts"       -> JsNumber(repairsAndMaintenanceCosts),
+            "loanInterestOrOtherFinancialCost" -> JsNumber(loanInterest),
+            "otherProfessionalFees"            -> JsNumber(otherProfessionalFee),
+            "costsOfServicesProvided"          -> JsNumber(costsOfServicesProvided),
+            "propertyBusinessTravelCosts"      -> JsNumber(propertyBusinessTravelCost),
+            "otherAllowablePropertyExpenses"   -> JsNumber(otherAllowablePropertyExpenses)
+          )
+        )
+      )
+      val application = applicationBuilder(userAnswers = Some(answers), isAgent = true)
+        .overrides(bind[PropertySubmissionService].toInstance(propertySubmissionService))
+        .build()
+
+      when(
+        propertySubmissionService
+          .saveJourneyAnswers(
+            ArgumentMatchers.eq(context),
+            ArgumentMatchers.eq(
+              RentARoomExpenses(
+                consolidatedExpenses = None,
+                rentsRatesAndInsurance = Some(rentsRatesAndInsurance),
+                repairsAndMaintenanceCosts = Some(repairsAndMaintenanceCosts),
+                loanInterestOrOtherFinancialCost = Some(loanInterest),
+                otherProfessionalFees = Some(otherProfessionalFee),
+                costsOfServicesProvided = Some(costsOfServicesProvided),
+                propertyBusinessTravelCosts = Some(propertyBusinessTravelCost),
+                otherAllowablePropertyExpenses = Some(otherAllowablePropertyExpenses)
+              )
+            )
+          )(
+            any(),
+            any()
+          )
+      ) thenReturn Future(Right())
 
       running(application) {
         val request = FakeRequest(POST, routes.ExpensesCheckYourAnswersRRController.onSubmit(taxYear).url)
