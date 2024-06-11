22bd5aa7027b03b11a809d3c7d02eacfedb3c87e
William Sharkey <90686675+willjshark@users.noreply.github.com>
2024-06-11 12:03:05 +0100

    Added Welsh for ESBA (#202)

--- app/pages/ukrentaroom/allowances/RaRAnnualInvestmentAllowancePage.scala
+++ app/pages/ukrentaroom/allowances/RaRAnnualInvestmentAllowancePage.scala
@@ -16,12 +16,12 @@
 
 package pages.ukrentaroom.allowances
 
-import pages.QuestionPage
+import pages.{PageConstants, QuestionPage}
 import play.api.libs.json.JsPath
 
 case object RaRAnnualInvestmentAllowancePage extends QuestionPage[BigDecimal] {
 
-  override def path: JsPath = JsPath \ toString
+  override def path: JsPath = JsPath \ PageConstants.rentARoomAllowance \ toString
 
-  override def toString: String = "raRAnnualInvestmentAllowance"
+  override def toString: String = "annualInvestmentAllowance"
 }
