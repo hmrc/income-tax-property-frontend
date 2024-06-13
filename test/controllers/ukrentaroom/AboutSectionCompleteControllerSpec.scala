b80fd131499d6f585572440aa44ff677d0503c56
Costas Mantzoukas <159533843+cmantzoukashmrc@users.noreply.github.com>
2024-06-13 23:27:05 +0100

    SASS-8474 fix test

--- test/controllers/ukrentaroom/AboutSectionCompleteControllerSpec.scala
+++ test/controllers/ukrentaroom/AboutSectionCompleteControllerSpec.scala
@@ -27,7 +27,6 @@ import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
 import pages.ukrentaroom.AboutSectionCompletePage
 import play.api.inject.bind
-import play.api.libs.json.Json
 import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
