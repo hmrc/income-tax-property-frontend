5c32d0ca565042f1911f359a0c08b572ab0fa24c
vivekhmrc <vivek.kumar@digital.hmrc.gov.uk>
2024-08-19 10:40:58 +0100

    SASS-8959: Added parameterized tests and also added random pick for Individual and Agent

--- test/controllers/allowances/ReplacementOfDomesticGoodsControllerSpec.scala
+++ test/controllers/allowances/ReplacementOfDomesticGoodsControllerSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,11 +18,13 @@ package controllers.allowances
 
 import base.SpecBase
 import forms.allowances.ReplacementOfDomesticGoodsFormProvider
-import models.{NormalMode, Rentals, UserAnswers}
+import models.{NormalMode, PropertyType, Rentals, RentalsRentARoom, UserAnswers}
 import navigation.{FakeNavigator, Navigator}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
+import org.scalatest.prop.Tables.Table
 import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks.forAll
 import pages.allowances.ReplacementOfDomesticGoodsPage
 import play.api.data.Form
 import play.api.inject.bind
@@ -33,130 +35,158 @@ import repositories.SessionRepository
 import views.html.allowances.ReplacementOfDomesticGoodsView
 
 import scala.concurrent.Future
+import scala.util.Random
 
 class ReplacementOfDomesticGoodsControllerSpec extends SpecBase with MockitoSugar {
 
   val formProvider = new ReplacementOfDomesticGoodsFormProvider()
-  private val isAgentMessageKey = "individual"
-  val form: Form[BigDecimal] = formProvider(isAgentMessageKey)
-
-  def onwardRoute = Call("GET", "/foo")
-
+  val taxYear = 2024
   val validAnswer: BigDecimal = BigDecimal(0)
-  val taxYear = 2023
 
-  lazy val replacementOfDomesticGoodsControllerRoute = routes.ReplacementOfDomesticGoodsController.onPageLoad(taxYear, NormalMode, Rentals).url
+  private val individualAgent = Array("individual", "agent")
+  private val individualOrAgent = individualAgent(Random.nextInt(individualAgent.length))
+  private val isAgent = individualOrAgent.equals("agent")
 
-  "ReplacementOfDomesticGoodsController Controller" - {
+  private val form: Form[BigDecimal] = formProvider(individualOrAgent)
+  private val scenarios = Table[PropertyType, String](
+    ("property type", "type definition"),
+    (Rentals, "rentals"),
+    (RentalsRentARoom, "rentalsAndRaR")
+  )
 
-    "must return OK and the correct view for a GET" in {
+  private def onwardRoute = Call("GET", "/foo")
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false).build()
+  forAll(scenarios) { (propertyType: PropertyType, propertyTypeDefinition: String) =>
+    lazy val replacementOfDomesticGoodsControllerRoute: String =
+      routes.ReplacementOfDomesticGoodsController.onPageLoad(taxYear, NormalMode, propertyType).url
 
-      running(application) {
-        val request = FakeRequest(GET, replacementOfDomesticGoodsControllerRoute)
+    s"ReplacementOfDomesticGoodsController for property type: $propertyTypeDefinition and for an $individualOrAgent " - {
 
-        val result = route(application, request).value
+      s"must return OK and the correct view for a GET " in {
 
-        val view = application.injector.instanceOf[ReplacementOfDomesticGoodsView]
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = isAgent).build()
 
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, taxYear, isAgentMessageKey, NormalMode, Rentals)(request, messages(application)).toString
+        running(application) {
+          val request = FakeRequest(GET, replacementOfDomesticGoodsControllerRoute)
+
+          val result = route(application, request).value
+
+          val view = application.injector.instanceOf[ReplacementOfDomesticGoodsView]
+
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual view(form, taxYear, individualOrAgent, NormalMode, propertyType)(
+            request,
+            messages(application)
+          ).toString
+        }
       }
-    }
 
-    "must populate the view correctly on a GET when the question has previously been answered" in {
+      "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val userAnswers = UserAnswers(userAnswersId).set(ReplacementOfDomesticGoodsPage(Rentals), validAnswer).success.value
+        val userAnswers =
+          UserAnswers(userAnswersId).set(ReplacementOfDomesticGoodsPage(propertyType), validAnswer).success.value
 
-      val application = applicationBuilder(userAnswers = Some(userAnswers), isAgent = false).build()
+        val application = applicationBuilder(userAnswers = Some(userAnswers), isAgent = isAgent).build()
 
-      running(application) {
-        val request = FakeRequest(GET, replacementOfDomesticGoodsControllerRoute)
+        running(application) {
+          val request = FakeRequest(GET, replacementOfDomesticGoodsControllerRoute)
 
-        val view = application.injector.instanceOf[ReplacementOfDomesticGoodsView]
+          val view = application.injector.instanceOf[ReplacementOfDomesticGoodsView]
 
-        val result = route(application, request).value
+          val result = route(application, request).value
 
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(validAnswer), taxYear, isAgentMessageKey, NormalMode, Rentals)(request, messages(application)).toString
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual view(
+            form.fill(validAnswer),
+            taxYear,
+            individualOrAgent,
+            NormalMode,
+            propertyType
+          )(
+            request,
+            messages(application)
+          ).toString
+        }
       }
-    }
 
-    "must redirect to the next page when valid data is submitted" in {
+      "must redirect to the next page when valid data is submitted" in {
 
-      val mockSessionRepository = mock[SessionRepository]
+        val mockSessionRepository = mock[SessionRepository]
 
-      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+        when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
 
-      val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false)
-          .overrides(
-            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
-            bind[SessionRepository].toInstance(mockSessionRepository)
-          )
-          .build()
+        val application =
+          applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = isAgent)
+            .overrides(
+              bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+              bind[SessionRepository].toInstance(mockSessionRepository)
+            )
+            .build()
 
-      running(application) {
-        val request =
-          FakeRequest(POST, replacementOfDomesticGoodsControllerRoute)
-            .withFormUrlEncodedBody(("replacementOfDomesticGoodsAmount", validAnswer.toString))
+        running(application) {
+          val request =
+            FakeRequest(POST, replacementOfDomesticGoodsControllerRoute)
+              .withFormUrlEncodedBody(("replacementOfDomesticGoodsAmount", validAnswer.toString))
 
-        val result = route(application, request).value
+          val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual onwardRoute.url
+          status(result) mustEqual SEE_OTHER
+          redirectLocation(result).value mustEqual onwardRoute.url
+        }
       }
-    }
 
-    "must return a Bad Request and errors when invalid data is submitted" in {
+      "must return a Bad Request and errors when invalid data is submitted" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false).build()
+        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = isAgent).build()
 
-      running(application) {
-        val request =
-          FakeRequest(POST, replacementOfDomesticGoodsControllerRoute)
-            .withFormUrlEncodedBody(("replacementOfDomesticGoodsAmount", "invalid value"))
+        running(application) {
+          val request =
+            FakeRequest(POST, replacementOfDomesticGoodsControllerRoute)
+              .withFormUrlEncodedBody(("replacementOfDomesticGoodsAmount", "invalid value"))
 
-        val boundForm = form.bind(Map("replacementOfDomesticGoodsAmount" -> "invalid value"))
+          val boundForm = form.bind(Map("replacementOfDomesticGoodsAmount" -> "invalid value"))
 
-        val view = application.injector.instanceOf[ReplacementOfDomesticGoodsView]
+          val view = application.injector.instanceOf[ReplacementOfDomesticGoodsView]
 
-        val result = route(application, request).value
+          val result = route(application, request).value
 
-        status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, taxYear, isAgentMessageKey, NormalMode, Rentals)(request, messages(application)).toString
+          status(result) mustEqual BAD_REQUEST
+          contentAsString(result) mustEqual view(boundForm, taxYear, individualOrAgent, NormalMode, propertyType)(
+            request,
+            messages(application)
+          ).toString
+        }
       }
-    }
 
-    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+      "must redirect to Journey Recovery for a GET if no existing data is found" in {
 
-      val application = applicationBuilder(userAnswers = None, isAgent = false).build()
+        val application = applicationBuilder(userAnswers = None, isAgent = isAgent).build()
 
-      running(application) {
-        val request = FakeRequest(GET, replacementOfDomesticGoodsControllerRoute)
+        running(application) {
+          val request = FakeRequest(GET, replacementOfDomesticGoodsControllerRoute)
 
-        val result = route(application, request).value
+          val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+          status(result) mustEqual SEE_OTHER
+          redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+        }
       }
-    }
 
-    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+      "must redirect to Journey Recovery for a POST if no existing data is found" in {
 
-      val application = applicationBuilder(userAnswers = None, isAgent = false).build()
+        val application = applicationBuilder(userAnswers = None, isAgent = isAgent).build()
 
-      running(application) {
-        val request =
-          FakeRequest(POST, replacementOfDomesticGoodsControllerRoute)
-            .withFormUrlEncodedBody(("replacementOfDomesticGoodsAmount", validAnswer.toString))
+        running(application) {
+          val request =
+            FakeRequest(POST, replacementOfDomesticGoodsControllerRoute)
+              .withFormUrlEncodedBody(("replacementOfDomesticGoodsAmount", validAnswer.toString))
 
-        val result = route(application, request).value
+          val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
+          status(result) mustEqual SEE_OTHER
 
-        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+          redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+        }
       }
     }
   }
