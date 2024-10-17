f9178aafa915a7d3e1e0f3417de118e13dd35a17
stephen-oppong-beduh <56881842+stephen-oppong-beduh@users.noreply.github.com>
2024-10-17 15:28:27 +0100

    further

--- app/controllers/foreign/TotalIncomeController.scala
+++ app/controllers/foreign/TotalIncomeController.scala
@@ -1,8 +1,23 @@
-package controllers
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.foreign
 
 import controllers.actions._
 import forms.TotalIncomeFormProvider
-import javax.inject.Inject
 import models.Mode
 import navigation.Navigator
 import pages.TotalIncomePage
@@ -10,8 +25,9 @@ import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.TotalIncomeView
+import views.html.foreign.TotalIncomeView
 
+import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 class TotalIncomeController @Inject()(
@@ -28,7 +44,7 @@ class TotalIncomeController @Inject()(
 
   val form = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad(taxYear: Int, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
 
       val preparedForm = request.userAnswers.get(TotalIncomePage) match {
@@ -36,21 +52,21 @@ class TotalIncomeController @Inject()(
         case Some(value) => form.fill(value)
       }
 
-      Ok(view(preparedForm, mode))
+      Ok(view(preparedForm, taxYear, mode))
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+  def onSubmit(taxYear: Int, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
 
       form.bindFromRequest().fold(
         formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors, mode))),
+          Future.successful(BadRequest(view(formWithErrors, taxYear, mode))),
 
         value =>
           for {
             updatedAnswers <- Future.fromTry(request.userAnswers.set(TotalIncomePage, value))
             _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(TotalIncomePage, mode, updatedAnswers))
+          } yield Redirect(navigator.nextPage(TotalIncomePage, taxYear, mode, request.userAnswers, updatedAnswers))
       )
   }
 }
--- app/viewmodels/checkAnswers/TotalIncomeSummary.scala
+++ app/viewmodels/checkAnswers/TotalIncomeSummary.scala
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package viewmodels.checkAnswers
 
-import controllers.routes
 import models.{CheckMode, UserAnswers}
 import pages.TotalIncomePage
 import play.api.i18n.Messages
@@ -12,7 +27,7 @@ import viewmodels.implicits._
 
 object TotalIncomeSummary  {
 
-  def row(answers: UserAnswers)(implicit messages: Messages): Option[SummaryListRow] =
+  def row(taxYear: Int, answers: UserAnswers)(implicit messages: Messages): Option[SummaryListRow] =
     answers.get(TotalIncomePage).map {
       answer =>
 
@@ -26,7 +41,7 @@ object TotalIncomeSummary  {
           key     = "totalIncome.checkYourAnswersLabel",
           value   = value,
           actions = Seq(
-            ActionItemViewModel("site.change", routes.TotalIncomeController.onPageLoad(CheckMode).url)
+            ActionItemViewModel("site.change", controllers.foreign.routes.TotalIncomeController.onPageLoad(taxYear, CheckMode).url)
               .withVisuallyHiddenText(messages("totalIncome.change.hidden"))
           )
         )
