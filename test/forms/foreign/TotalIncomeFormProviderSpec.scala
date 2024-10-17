df3cba9294cde48e4afa4de881cb92e8bf431360
stephen-oppong-beduh <56881842+stephen-oppong-beduh@users.noreply.github.com>
2024-10-17 14:03:19 +0100

    SASS-9520 Added foreign properties total income

---
+++ test/forms/foreign/TotalIncomeFormProviderSpec.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.foreign
+
+import forms.behaviours.OptionFieldBehaviours
+import models.ForeignTotalIncome
+import org.scalatest.prop.TableFor1
+import play.api.data.FormError
+
+class TotalIncomeFormProviderSpec extends OptionFieldBehaviours {
+
+  val scenarios: TableFor1[String] = Table[String]("individualOrAgent", "individual", "agent")
+
+  forAll(scenarios) { (individualOrAgent: String) =>
+    val form = new TotalIncomeFormProvider()(individualOrAgent)
+
+    s".foreignTotalIncome for an $individualOrAgent" - {
+
+      val fieldName = "foreignTotalIncome"
+      val requiredKey = s"foreignTotalIncome.error.required.$individualOrAgent"
+
+      behave like optionsField[ForeignTotalIncome](
+        form,
+        fieldName,
+        validValues  = ForeignTotalIncome.values,
+        invalidError = FormError(fieldName, "error.invalid")
+      )
+
+      behave like mandatoryField(
+        form,
+        fieldName,
+        requiredError = FormError(fieldName, requiredKey)
+      )
+    }
+  }
+
+
+
+}
