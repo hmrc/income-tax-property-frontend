ad01fb97786738e767d3d2d12d4eeb6a7bcb1a98
stephen-oppong-beduh <56881842+stephen-oppong-beduh@users.noreply.github.com>
2024-10-31 16:01:44 +0000

    SASS-9591 Added foreign tax income

---
+++ app/viewmodels/checkAnswers/foreign/ForeignIncomeTaxSummary.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.checkAnswers.foreign
+
+import controllers.foreign.routes
+import models.{CheckMode, ForeignIncomeTax, UserAnswers}
+import pages.foreign.ForeignIncomeTaxPage
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Content
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import viewmodels.checkAnswers.FormatUtils.bigDecimalCurrency
+import viewmodels.govuk.summarylist._
+import viewmodels.implicits._
+
+object ForeignIncomeTaxSummary {
+
+  def row(taxYear: Int, individualOrAgent: String, countryCode: String, answers: UserAnswers)(implicit messages: Messages): Option[SummaryListRow] =
+    answers.get(ForeignIncomeTaxPage(countryCode)).flatMap { answer =>
+      val row: Content => Option[SummaryListRow] = content => Some(
+        SummaryListRowViewModel(
+          key = s"foreignIncomeTax.checkYourAnswersLabel.$individualOrAgent",
+          value = ValueViewModel(content),
+          actions = Seq(
+            ActionItemViewModel("site.change", routes.ForeignIncomeTaxController.onPageLoad(taxYear, countryCode, CheckMode).url)
+              .withVisuallyHiddenText(messages(s"foreignIncomeTax.change.hidden.$individualOrAgent"))
+          )
+        )
+      )
+
+      answer match {
+        case ForeignIncomeTax(true, Some(amount)) =>
+          row(bigDecimalCurrency(amount))
+        case ForeignIncomeTax(false, _) =>
+          row("site.no")
+        case _ =>
+          Option.empty[SummaryListRow]
+      }
+    }
+}
