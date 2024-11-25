a16cd3175f77ae079d797ef1316f264b29fb06d5
RGlossop <75027918+RGlossop@users.noreply.github.com>
2024-11-25 18:29:57 +0000

    SASS-9700 added foreign non residential property finance costs

---
+++ test/forms/foreign/expenses/ForeignNonResidentialPropertyFinanceCostsFormProviderSpec.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.foreign.expenses
+
+import forms.behaviours.CurrencyFieldBehaviours
+import play.api.data.FormError
+
+class ForeignNonResidentialPropertyFinanceCostsFormProviderSpec extends CurrencyFieldBehaviours {
+
+  val form = new ForeignNonResidentialPropertyFinanceCostsFormProvider()("individual")
+
+  ".foreignNonResidentialPropertyFinanceCostsAmount" - {
+
+    val fieldName = "foreignNonResidentialPropertyFinanceCostsAmount"
+
+    val minimum = 0
+    val maximum = 100000000
+
+    val validDataGenerator = intsInRangeWithCommas(minimum, maximum)
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      validDataGenerator
+    )
+
+    behave like currencyField(
+      form,
+      fieldName,
+      nonNumericError = FormError(fieldName, "foreignNonResidentialPropertyFinanceCosts.error.nonNumeric.individual"),
+      twoDecimalPlacesError = FormError(fieldName, "foreignNonResidentialPropertyFinanceCosts.error.nonNumeric.individual")
+    )
+
+    behave like currencyFieldWithRange(
+      form,
+      fieldName,
+      minimum = minimum,
+      maximum = maximum,
+      expectedError = FormError(fieldName, "foreignNonResidentialPropertyFinanceCosts.error.outOfRange", Seq(minimum, maximum))
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, "foreignNonResidentialPropertyFinanceCosts.error.required.individual")
+    )
+  }
+}
