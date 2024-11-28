70088408e0a52364fe941678d22155cd1a80b7ef
Stuart Proctor <112641125+StuartJamesProctor@users.noreply.github.com>
2024-11-28 16:47:49 +0000

    SASS-9704 completed view and working on controller

--- app/controllers/foreign/expenses/ForeignPropertyExpensesCheckYourAnswersController.scala
+++ app/controllers/foreign/expenses/ForeignPropertyExpensesCheckYourAnswersController.scala
@@ -1,56 +1,60 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.foreign.expenses
 
 import controllers.actions._
 import forms.ForeignPropertyExpensesCheckYourAnswersFormProvider
 import views.html.foreign.expenses.ForeignPropertyExpensesCheckYourAnswersView
+import controllers.foreign.expenses.routes.ForeignPropertyExpensesCheckYourAnswersController
+
 import javax.inject.Inject
-import models.Mode
-import navigation.Navigator
-import pages.ForeignPropertyExpensesCheckYourAnswersPage
+import navigation.ForeignPropertyNavigator
 import play.api.i18n.{MessagesApi, I18nSupport}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import viewmodels.govuk.all.SummaryListViewModel
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class ForeignPropertyExpensesCheckYourAnswersController @Inject()(
                                          override val messagesApi: MessagesApi,
-                                         sessionRepository: SessionRepository,
-                                         navigator: Navigator,
+                                         navigator: ForeignPropertyNavigator,
                                          identify: IdentifierAction,
                                          getData: DataRetrievalAction,
                                          requireData: DataRequiredAction,
-                                         formProvider: ForeignPropertyExpensesCheckYourAnswersFormProvider,
                                          val controllerComponents: MessagesControllerComponents,
                                          view: ForeignPropertyExpensesCheckYourAnswersView
                                  )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
-  val form = formProvider()
-
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad(taxYear: Int, countryCode: String): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
 
-      val preparedForm = request.userAnswers.get(ForeignPropertyExpensesCheckYourAnswersPage) match {
-        case None => form
-        case Some(value) => form.fill(value)
-      }
+      val list = SummaryListViewModel(
+        rows = Seq(
 
-      Ok(view(preparedForm, mode))
+        ).flatten
+      )
+
+      Ok(view(list, taxYear, countryCode))
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+  def onSubmit(taxYear: Int, countryCode: String): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
-
-      form.bindFromRequest().fold(
-        formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors, mode))),
-
-        value =>
-          for {
-            updatedAnswers <- Future.fromTry(request.userAnswers.set(ForeignPropertyExpensesCheckYourAnswersPage, value))
-            _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(ForeignPropertyExpensesCheckYourAnswersPage, mode, updatedAnswers))
-      )
+      Future.successful(Redirect(ForeignPropertyExpensesCheckYourAnswersController.onPageLoad(taxYear, countryCode)))
   }
 }
