4c37730a3fbb1b5615a70d48a5484c432d79cd88
RGlossop <75027918+RGlossop@users.noreply.github.com>
2024-12-18 15:05:31 +0000

    SASS-9720: allowances path added foreign other capital allowances

--- app/pages/PageConstants.scala
+++ app/pages/PageConstants.scala
@@ -63,6 +63,8 @@ object PageConstants {
 
   val foreignPropertyExpensesPath: PropertyType => String = labelForPropertyType(_, "Expenses")
 
+  val foreignPropertyAllowancesPath: PropertyType => String = labelForPropertyType(_, "ForeignAllowances")
+
   val foreignPropertySectionFinished: String = "foreignPropertySectionFinished"
   
 }
