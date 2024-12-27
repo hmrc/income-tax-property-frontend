533462e550e12cc3f4cce3a84f79ff379f6dfd0e
iyadi <7164084+iyadi@users.noreply.github.com>
2024-12-27 14:27:03 +0000

    SASS-10762:7. UK & Foreign Property Mini Journey - Create 'Claim expenses or rent a room relief' page -Implement new page and integrate journey

---
+++ app/controllers/ukandforeignproperty/ClaimExpensesOrReliefController.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.ukandforeignproperty
+
+import controllers.actions._
+import forms.ukandforeignproperty.ClaimExpensesOrReliefFormProvider
+import models.{Mode, PropertyType}
+import pages.ukandforeignproperty.ClaimExpensesOrReliefPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.ukandforeignproperty.ClaimExpensesOrReliefView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class ClaimExpensesOrReliefController @Inject()(
+                                                 override val messagesApi: MessagesApi,
+                                                 identify: IdentifierAction,
+                                                 getData: DataRetrievalAction,
+                                                 requireData: DataRequiredAction,
+                                                 formProvider: ClaimExpensesOrReliefFormProvider,
+                                                 val controllerComponents: MessagesControllerComponents,
+                                                 view: ClaimExpensesOrReliefView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad(taxYear: Int, mode: Mode, propertyType: PropertyType): Action[AnyContent] =
+    (identify andThen getData andThen requireData).async { implicit request =>
+      val form = formProvider(request.user.isAgentMessageKey, BigDecimal(0))
+      val preparedForm = request.userAnswers.get(ClaimExpensesOrReliefPage(propertyType)) match {
+        case None        => form
+        case Some(value) => form.fill(value)
+      }
+      Future.successful(
+        Ok(view(preparedForm, taxYear, mode, request.user.isAgentMessageKey, propertyType))
+      )
+    }
+
+  def onSubmit(taxYear: Int, mode: Mode, propertyType: PropertyType): Action[AnyContent] =
+    (identify andThen getData andThen requireData).async { implicit request =>
+      ???
+    }
+
+}
+
+case object NotFoundException extends Exception("TotalIncomeAmount is not present in userAnswers")
