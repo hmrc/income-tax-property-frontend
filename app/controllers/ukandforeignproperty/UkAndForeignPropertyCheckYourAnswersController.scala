ea9cd8c152c355e000d106189d964849414289eb
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2025-01-02 11:01:36 +0000

    [SASS-10765]OnPageLoad, routes and view added

---
+++ app/controllers/ukandforeignproperty/UkAndForeignPropertyCheckYourAnswersController.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.ukandforeignproperty
+
+import controllers.actions._
+import models.Mode
+import play.api.Logging
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+//import service.PropertySubmissionService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import viewmodels.checkAnswers.ReportIncomeSummary
+import viewmodels.checkAnswers.ukandforeignproperty.TotalPropertyIncomeSummary
+import viewmodels.govuk.all.SummaryListViewModel
+import views.html.ukandforeignproperty.UkAndForeignPropertyCheckYourAnswersView
+
+import javax.inject.Inject
+//import scala.concurrent.ExecutionContext
+
+class UkAndForeignPropertyCheckYourAnswersController @Inject() (
+                                                       override val messagesApi: MessagesApi,
+                                                       identify: IdentifierAction,
+                                                       getData: DataRetrievalAction,
+                                                       requireData: DataRequiredAction,
+                                                       val controllerComponents: MessagesControllerComponents,
+                                                       //propertySubmissionService: PropertySubmissionService,
+                                                       view: UkAndForeignPropertyCheckYourAnswersView
+                                                     )//(implicit ec: ExecutionContext)
+  extends FrontendBaseController with I18nSupport with Logging {
+
+  def onPageLoad(taxYear: Int, mode: Mode): Action[AnyContent] =
+    (identify andThen getData andThen requireData) { implicit request =>
+      val list = SummaryListViewModel(
+        rows = Seq(
+          TotalPropertyIncomeSummary.row(taxYear = taxYear, answers = request.userAnswers),
+          ReportIncomeSummary.row(taxYear = taxYear, answers = request.userAnswers)
+        ).flatten
+      )
+
+      Ok(view(list, taxYear, mode))
+    }
+}
---
+++ test/views/UkAndForeignPropertyCheckYourAnswersViewSpec.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import base.SpecBase
+import models.{Mode, NormalMode}
+import org.scalatest.matchers.must.Matchers
+import play.api.Application
+import play.api.i18n.{Messages, MessagesApi}
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.mvc.Request
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{GET, contentAsString, defaultAwaitTimeout}
+import play.twirl.api.Html
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryList
+import views.html.ukandforeignproperty.UkAndForeignPropertyCheckYourAnswersView
+
+import java.time.LocalDate
+
+class UkAndForeignPropertyCheckYourAnswersViewSpec extends SpecBase with Matchers {
+
+  val application: Application = new GuiceApplicationBuilder().build()
+
+  implicit val messages: Messages = application.injector.instanceOf[MessagesApi].preferred(Seq.empty)
+
+  val view: UkAndForeignPropertyCheckYourAnswersView = application.injector.instanceOf[UkAndForeignPropertyCheckYourAnswersView]
+
+  def createView(list: SummaryList,  taxYear: Int, mode: Mode)(implicit request: Request[_]): Html = {
+    view(list, taxYear, mode)(request, messages)
+  }
+
+  "UkAndForeignPropertyCheckYourAnswersView" - {
+
+    "render correctly" in {
+      val list = SummaryList(Seq.empty)
+      val mode = NormalMode
+      val taxYear: Int = LocalDate.now.getYear
+
+      val request = FakeRequest(GET, "/")
+      val result = createView(list, taxYear, mode)(request)
+
+      contentAsString(result) must include(messages("checkYourAnswers.title"))
+      contentAsString(result) must include(messages("taxYear.caption"))
+      contentAsString(result) must include(messages("checkYourAnswers.heading"))
+      contentAsString(result) must include(messages("site.saveAndContinue"))
+    }
+  }
+
+}
