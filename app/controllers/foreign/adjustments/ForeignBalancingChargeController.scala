60d82dec1b8f0d8429dfbe815cc7a116c33b51ca
PraneethN-Kotaru <178334044+PraneethN-Kotaru@users.noreply.github.com>
2025-01-03 14:39:27 +0000

    SASS-9758: Adjustments: Create 'Balancing charge' page

---
+++ app/controllers/foreign/adjustments/ForeignBalancingChargeController.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.foreign.adjustments
+
+import controllers.actions._
+import forms.foreign.adjustments.ForeignBalancingChargeFormProvider
+import models.{Mode, PropertyType}
+import navigation.{ForeignPropertyNavigator, Navigator}
+import pages.foreign.adjustments.ForeignBalancingChargePage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.foreign.adjustments.ForeignBalancingChargeView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class ForeignBalancingChargeController @Inject()(
+                                         override val messagesApi: MessagesApi,
+                                         sessionRepository: SessionRepository,
+                                         navigator: Navigator,
+                                         identify: IdentifierAction,
+                                         getData: DataRetrievalAction,
+                                         requireData: DataRequiredAction,
+                                         formProvider: BalancingChargeFormProvider,
+                                         val controllerComponents: MessagesControllerComponents,
+                                         view: BalancingChargeView
+                                       )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad(taxYear: Int, countryCode: String, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+      val form = formProvider(request.user.isAgentMessageKey)
+      val preparedForm = request.userAnswers.get(ForeignBalancingChargePage(countryCode)) match {
+        case None => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, taxYear, mode, request.user.isAgentMessageKey, propertyType))
+  }
+
+  def onSubmit(taxYear: Int, countryCode: String, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+    implicit request =>
+      val form = formProvider(request.user.isAgentMessageKey)
+      form.bindFromRequest().fold(
+        formWithErrors =>
+          Future.successful(BadRequest(view(formWithErrors, taxYear, mode, request.user.isAgentMessageKey, propertyType))),
+
+        value =>
+          for {
+            updatedAnswers <- Future.fromTry(request.userAnswers.set(ForeignBalancingChargePage(countryCode), value))
+            _ <- sessionRepository.set(updatedAnswers)
+          } yield Redirect(navigator.nextPage(ForeignBalancingChargePage(countryCode), taxYear, mode, request.userAnswers, updatedAnswers))
+      )
+  }
+}
---
+++ test/controllers/foreign/adjustments/ForeignBalancingChargeControllerSpec.scala
@@ -0,0 +1,246 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.foreign.adjustments
+
+import base.SpecBase
+import forms.foreign.adjustments.ForeignBalancingChargeFormProvider
+import models.{BalancingCharge, NormalMode, UserAnswers}
+import navigation.{FakeForeignPropertyNavigator, ForeignPropertyNavigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.foreign.adjustments.ForeignBalancingChargePage
+import play.api.data.Form
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import repositories.SessionRepository
+import views.html.foreign.adjustments.ForeignBalancingChargeView
+
+import java.time.LocalDate
+import scala.concurrent.Future
+
+class ForeignBalancingChargeControllerSpec extends SpecBase with MockitoSugar {
+
+  def onwardRoute: Call = Call("GET", "/balanceCharge")
+
+  val taxYear: Int = 2023
+  val countryCode = "AUS"
+  lazy val rentalsBalancingChargeRoute: String =
+    routes.BalancingChargeController.onPageLoad(taxYear, NormalMode, Rentals).url
+  lazy val rentalsRentARoomBalancingChargeRoute: String =
+    routes.BalancingChargeController.onPageLoad(taxYear, NormalMode, RentalsRentARoom).url
+
+  val formProvider = new BalancingChargeFormProvider()
+  val form: Form[BalancingCharge] = formProvider("agent")
+
+  "BalancingCharge Controller" - {
+
+    "must return OK and the correct view for a GET when an individual for both Rentals and Rentals and Rent a Room journeys" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false).build()
+      val view = application.injector.instanceOf[BalancingChargeView]
+
+      running(application) {
+        // Rentals
+        val rentalsRequest = FakeRequest(GET, rentalsBalancingChargeRoute)
+        val rentalsResult = route(application, rentalsRequest).value
+
+        status(rentalsResult) mustEqual OK
+        contentAsString(rentalsResult) mustEqual view(form, taxYear, NormalMode, "individual", Rentals)(
+          rentalsRequest,
+          messages(application)
+        ).toString
+
+        // Rentals and Rent a Room
+        val rentalsRentARoomRequest = FakeRequest(GET, rentalsRentARoomBalancingChargeRoute)
+        val rentalsRentARoomResult = route(application, rentalsRentARoomRequest).value
+
+        status(rentalsRentARoomResult) mustEqual OK
+        contentAsString(rentalsRentARoomResult) mustEqual view(
+          form,
+          taxYear,
+          NormalMode,
+          "individual",
+          RentalsRentARoom
+        )(
+          rentalsRentARoomRequest,
+          messages(application)
+        ).toString
+      }
+    }
+
+    "must return OK and the correct view for a GET when an agent for both Rentals and Rentals and Rent a Room journeys" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = true).build()
+      val view = application.injector.instanceOf[BalancingChargeView]
+
+      running(application) {
+
+        // Rentals
+        val rentalsRequest = FakeRequest(GET, rentalsBalancingChargeRoute)
+        val rentalsResult = route(application, rentalsRequest).value
+
+        status(rentalsResult) mustEqual OK
+        contentAsString(rentalsResult) mustEqual view(form, taxYear, NormalMode, "agent", Rentals)(
+          rentalsRequest,
+          messages(application)
+        ).toString
+
+        // Rentals and Rent a Room
+        val rentalsRentARoomRequest = FakeRequest(GET, rentalsRentARoomBalancingChargeRoute)
+        val rentalsRentARoomResult = route(application, rentalsRentARoomRequest).value
+
+        status(rentalsRentARoomResult) mustEqual OK
+        contentAsString(rentalsRentARoomResult) mustEqual view(form, taxYear, NormalMode, "agent", RentalsRentARoom)(
+          rentalsRentARoomRequest,
+          messages(application)
+        ).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered for both Rentals and Rentals and Rent a Room journeys" in {
+
+      // Rentals
+      val rentalsUserAnswers = UserAnswers(userAnswersId)
+        .set(BalancingChargePage(Rentals), BalancingCharge(balancingChargeYesNo = true, Some(7689.23)))
+        .success
+        .value
+
+      val rentalsApplication = applicationBuilder(userAnswers = Some(rentalsUserAnswers), isAgent = true).build()
+
+      running(rentalsApplication) {
+        val request = FakeRequest(GET, rentalsBalancingChargeRoute)
+        val view = rentalsApplication.injector.instanceOf[BalancingChargeView]
+        val result = route(rentalsApplication, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(
+          form.fill(BalancingCharge(balancingChargeYesNo = true, Some(7689.23))),
+          taxYear,
+          NormalMode,
+          "agent",
+          Rentals
+        )(request, messages(rentalsApplication)).toString
+      }
+
+      // Rentals and Rent a Room
+      val rentalsRentARoomUserAnswers = UserAnswers(userAnswersId)
+        .set(BalancingChargePage(RentalsRentARoom), BalancingCharge(balancingChargeYesNo = true, Some(7689.23)))
+        .success
+        .value
+
+      val rentalsRentARoomApplication =
+        applicationBuilder(userAnswers = Some(rentalsRentARoomUserAnswers), isAgent = true).build()
+
+      running(rentalsRentARoomApplication) {
+        val request = FakeRequest(GET, rentalsRentARoomBalancingChargeRoute)
+        val view = rentalsRentARoomApplication.injector.instanceOf[BalancingChargeView]
+        val result = route(rentalsRentARoomApplication, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(
+          form.fill(BalancingCharge(balancingChargeYesNo = true, Some(7689.23))),
+          taxYear,
+          NormalMode,
+          "agent",
+          RentalsRentARoom
+        )(request, messages(rentalsApplication)).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted for yes for both Rentals and Rentals and Rent a Room journeys" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = true)
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        // Rentals
+        val rentalsRequest =
+          FakeRequest(POST, rentalsBalancingChargeRoute)
+            .withFormUrlEncodedBody("balancingChargeYesNo" -> "false")
+
+        val result = route(application, rentalsRequest).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+
+        // Rentals and Rent a Room
+        val rentalsRentARoomRequest =
+          FakeRequest(POST, rentalsRentARoomBalancingChargeRoute)
+            .withFormUrlEncodedBody("balancingChargeYesNo" -> "false")
+
+        val rentalsResult = route(application, rentalsRentARoomRequest).value
+
+        status(rentalsResult) mustEqual SEE_OTHER
+        redirectLocation(rentalsResult).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted for both Rentals and Rentals and Rent a Room journeys" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = true).build()
+      val boundForm = form.bind(Map("balancingChargeAmount" -> "87.858585"))
+      val view = application.injector.instanceOf[BalancingChargeView]
+
+      running(application) {
+
+        // Rentals
+        val rentalsRequest =
+          FakeRequest(POST, rentalsBalancingChargeRoute)
+            .withFormUrlEncodedBody(("balancingChargeAmount", "87.858585"))
+
+        val rentalsResult = route(application, rentalsRequest).value
+
+        status(rentalsResult) mustEqual BAD_REQUEST
+        contentAsString(rentalsResult) mustEqual view(boundForm, taxYear, NormalMode, "agent", Rentals)(
+          rentalsRequest,
+          messages(application)
+        ).toString
+
+        // Rentals and Rent a Room
+        val rentalsRentARoomRequest =
+          FakeRequest(POST, rentalsRentARoomBalancingChargeRoute)
+            .withFormUrlEncodedBody(("balancingChargeAmount", "87.858585"))
+
+        val rentalsRentARoomResult = route(application, rentalsRentARoomRequest).value
+
+        status(rentalsRentARoomResult) mustEqual BAD_REQUEST
+        contentAsString(rentalsRentARoomResult) mustEqual view(
+          boundForm,
+          taxYear,
+          NormalMode,
+          "agent",
+          RentalsRentARoom
+        )(
+          rentalsRentARoomRequest,
+          messages(application)
+        ).toString
+      }
+    }
+  }
+}
