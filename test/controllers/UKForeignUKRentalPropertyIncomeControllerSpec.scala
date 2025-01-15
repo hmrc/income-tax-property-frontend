31fa5886db9aa831cd05f5ede3c37464cae1d2cc
Stuart Proctor <112641125+StuartJamesProctor@users.noreply.github.com>
2025-01-15 10:09:02 +0000

    SASS-10668 WIP Implementing 'How much income did you get from UK property rentals' page
    # Conflicts:
    #	conf/messages.cy
    #	conf/messages.en

---
+++ test/controllers/UKForeignUKRentalPropertyIncomeControllerSpec.scala
@@ -0,0 +1,144 @@
+package controllers
+
+import base.SpecBase
+import forms.uKRentalPropertyIncomeFormProvider
+import models.{NormalMode, UserAnswers}
+import navigation.{FakeNavigator, Navigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.uKRentalPropertyIncomePage
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import repositories.SessionRepository
+import views.html.uKRentalPropertyIncomeView
+
+import scala.concurrent.Future
+
+class uKRentalPropertyIncomeControllerSpec extends SpecBase with MockitoSugar {
+
+  val formProvider = new uKRentalPropertyIncomeFormProvider()
+  val form = formProvider()
+
+  def onwardRoute = Call("GET", "/foo")
+
+  val validAnswer = 0
+
+  lazy val uKRentalPropertyIncomeRoute = routes.uKRentalPropertyIncomeController.onPageLoad(NormalMode).url
+
+  "uKRentalPropertyIncome Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, uKRentalPropertyIncomeRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[uKRentalPropertyIncomeView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = UserAnswers(userAnswersId).set(uKRentalPropertyIncomePage, validAnswer).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, uKRentalPropertyIncomeRoute)
+
+        val view = application.injector.instanceOf[uKRentalPropertyIncomeView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form.fill(validAnswer), NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, uKRentalPropertyIncomeRoute)
+            .withFormUrlEncodedBody(("value", validAnswer.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, uKRentalPropertyIncomeRoute)
+            .withFormUrlEncodedBody(("value", "invalid value"))
+
+        val boundForm = form.bind(Map("value" -> "invalid value"))
+
+        val view = application.injector.instanceOf[uKRentalPropertyIncomeView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None, true).build()
+
+      running(application) {
+        val request = FakeRequest(GET, uKRentalPropertyIncomeRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None, true).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, uKRentalPropertyIncomeRoute)
+            .withFormUrlEncodedBody(("value", validAnswer.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
