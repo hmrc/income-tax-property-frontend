e0199d01502c0ce3a27c5eb0d32e0d34a055902d
Stuart Proctor <112641125+StuartJamesProctor@users.noreply.github.com>
2025-01-22 15:59:37 +0000

    SASS-10685 Fixed tests

--- test/controllers/ukandforeignproperty/ForeignRentalPropertyIncomeControllerSpec.scala
+++ test/controllers/ukandforeignproperty/ForeignRentalPropertyIncomeControllerSpec.scala
@@ -1,9 +1,26 @@
-package controllers
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.ukandforeignproperty
 
 import base.SpecBase
+import controllers.routes
 import forms.ukandforeignproperty.ForeignRentalPropertyIncomeFormProvider
-import models.{NormalMode, UserAnswers}
-import navigation.{FakeNavigator, Navigator}
+import models.{UserAnswers, NormalMode}
+import navigation.{Navigator, FakeNavigator}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
@@ -13,26 +30,30 @@ import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import repositories.SessionRepository
-import views.html.ForeignRentalPropertyIncomeView
+import controllers.ukandforeignproperty.routes.ForeignRentalPropertyIncomeController
+import play.api.data.Form
+import views.html.ukandforeignproperty.ForeignRentalPropertyIncomeView
 
 import scala.concurrent.Future
 
 class ForeignRentalPropertyIncomeControllerSpec extends SpecBase with MockitoSugar {
 
   val formProvider = new ForeignRentalPropertyIncomeFormProvider()
-  val form = formProvider()
+  val form: Form[BigDecimal] = formProvider()
+  val taxYear = 2024
+  val countryCode = "GRC"
 
   def onwardRoute = Call("GET", "/foo")
 
-  val validAnswer = 0
+  val validAnswer = BigDecimal(100)
 
-  lazy val foreignRentalPropertyIncomeRoute = routes.ForeignRentalPropertyIncomeController.onPageLoad(NormalMode).url
+  lazy val foreignRentalPropertyIncomeRoute = ForeignRentalPropertyIncomeController.onPageLoad(taxYear, countryCode, NormalMode).url
 
   "ForeignRentalPropertyIncome Controller" - {
 
     "must return OK and the correct view for a GET" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), false).build()
 
       running(application) {
         val request = FakeRequest(GET, foreignRentalPropertyIncomeRoute)
@@ -42,15 +63,15 @@ class ForeignRentalPropertyIncomeControllerSpec extends SpecBase with MockitoSug
         val view = application.injector.instanceOf[ForeignRentalPropertyIncomeView]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form, taxYear, countryCode, "individual", NormalMode)(request, messages(application)).toString
       }
     }
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val userAnswers = UserAnswers(userAnswersId).set(ForeignRentalPropertyIncomePage, validAnswer).success.value
+      val userAnswers = UserAnswers(userAnswersId).set(ForeignRentalPropertyIncomePage(countryCode), validAnswer).success.value
 
-      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(userAnswers), false).build()
 
       running(application) {
         val request = FakeRequest(GET, foreignRentalPropertyIncomeRoute)
@@ -60,7 +81,7 @@ class ForeignRentalPropertyIncomeControllerSpec extends SpecBase with MockitoSug
         val result = route(application, request).value
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(validAnswer), NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form.fill(validAnswer), taxYear, countryCode, "individual", NormalMode)(request, messages(application)).toString
       }
     }
 
@@ -71,7 +92,7 @@ class ForeignRentalPropertyIncomeControllerSpec extends SpecBase with MockitoSug
       when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
 
       val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        applicationBuilder(userAnswers = Some(emptyUserAnswers), false)
           .overrides(
             bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
             bind[SessionRepository].toInstance(mockSessionRepository)
@@ -81,7 +102,7 @@ class ForeignRentalPropertyIncomeControllerSpec extends SpecBase with MockitoSug
       running(application) {
         val request =
           FakeRequest(POST, foreignRentalPropertyIncomeRoute)
-            .withFormUrlEncodedBody(("value", validAnswer.toString))
+            .withFormUrlEncodedBody(("foreignRentalPropertyIncomeAmount", validAnswer.toString))
 
         val result = route(application, request).value
 
@@ -92,7 +113,7 @@ class ForeignRentalPropertyIncomeControllerSpec extends SpecBase with MockitoSug
 
     "must return a Bad Request and errors when invalid data is submitted" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), false).build()
 
       running(application) {
         val request =
@@ -106,7 +127,7 @@ class ForeignRentalPropertyIncomeControllerSpec extends SpecBase with MockitoSug
         val result = route(application, request).value
 
         status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(boundForm, taxYear, countryCode, "individual", NormalMode)(request, messages(application)).toString
       }
     }
 
