f247d04942fef57893ee4984d55337419e317853
Tom Burt-Gray <72973046+tbg2003@users.noreply.github.com>
2025-01-23 10:31:20 +0000

    SASS-10949: Foreign Property - Implement navigation functionality for Adjustments journey

--- app/viewmodels/checkAnswers/foreign/adjustments/ForeignWhenYouReportedTheLossSummary.scala
+++ app/viewmodels/checkAnswers/foreign/adjustments/ForeignWhenYouReportedTheLossSummary.scala
@@ -23,6 +23,7 @@ import play.api.i18n.Messages
 import play.twirl.api.HtmlFormat
 import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import viewmodels.checkAnswers.FormatUtils.valueCssClass
 import viewmodels.govuk.summarylist._
 import viewmodels.implicits._
 
@@ -36,7 +37,7 @@ object ForeignWhenYouReportedTheLossSummary  {
           HtmlContent(
             HtmlFormat.escape(messages(s"foreignWhenYouReportedTheLoss.$answer"))
           )
-        )
+        ).withCssClass(valueCssClass)
 
         SummaryListRowViewModel(
           key     = "foreignWhenYouReportedTheLoss.checkYourAnswersLabel",
