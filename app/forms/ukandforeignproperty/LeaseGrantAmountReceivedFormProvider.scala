500a396e04d2289f4eedbe526316b6362b83791a
Stuart Proctor <112641125+StuartJamesProctor@users.noreply.github.com>
2025-01-23 16:34:41 +0000

    SASS-10691: Create 'How much did you receive for the grant of a lease' (Foreign) page (#564)
    * SASS-10691 Implementing 'How much did you receive for the grant of a lease' (Foreign) page

---
+++ app/forms/ukandforeignproperty/LeaseGrantAmountReceivedFormProvider.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.ukandforeignproperty
+
+import forms.mappings.Mappings
+
+import javax.inject.Inject
+import play.api.data.Form
+
+class LeaseGrantAmountReceivedFormProvider @Inject() extends Mappings {
+
+  val minValue = 0
+  val maxValue = 100000000
+
+  def apply(): Form[BigDecimal] =
+    Form(
+      "leaseGrantReceivedAmount" -> currency(
+        "leaseGrantAmountReceived.error.required",
+        "leaseGrantAmountReceived.error.twoDecimalPlaces",
+        "leaseGrantAmountReceived.error.nonNumeric")
+          .verifying(inRange(BigDecimal(minValue), BigDecimal(maxValue), "leaseGrantAmountReceived.error.outOfRange"))
+    )
+}
