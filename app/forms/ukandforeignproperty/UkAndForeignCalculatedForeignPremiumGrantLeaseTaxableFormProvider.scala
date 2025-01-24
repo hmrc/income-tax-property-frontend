1f27b89c5f1214c975d00967c654b3bcebc0d974
RGlossop <75027918+RGlossop@users.noreply.github.com>
2025-01-24 15:28:14 +0000

    SASS-10689: Mini(Foreign) - Have you calculated the premium for the grant of a lease taxable amount

---
+++ app/forms/ukandforeignproperty/UkAndForeignCalculatedForeignPremiumGrantLeaseTaxableFormProvider.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.ukandforeignproperty
+
+import forms.mappings.Mappings
+import models.PremiumCalculated
+import play.api.data.Form
+import play.api.data.Forms.mapping
+import uk.gov.voa.play.form.ConditionalMappings.mandatoryIfTrue
+
+import javax.inject.Inject
+
+class UkAndForeignCalculatedForeignPremiumGrantLeaseTaxableFormProvider @Inject() extends Mappings {
+
+  def apply(individualOrAgent: String): Form[PremiumCalculated] =
+    Form[PremiumCalculated](
+      mapping(
+        "calculatedPremiumLeaseTaxable" -> boolean(s"calculatedPremiumLeaseTaxable.error.required.$individualOrAgent"),
+        "premiumsOfLeaseGrant" -> {
+          mandatoryIfTrue(
+            "calculatedPremiumLeaseTaxable",
+            currency(
+              s"premiumCalculated.amount.error.required.$individualOrAgent",
+              s"ukAndForeignCalculatedForeignPremiumGrantLeaseTaxable.amount.error.twoDecimalPlaces.${individualOrAgent}",
+              s"ukAndForeignCalculatedForeignPremiumGrantLeaseTaxable.amount.error.nonNumeric.${individualOrAgent}"
+            )
+              .verifying(inRange(BigDecimal(0), BigDecimal(100000000), "premiumCalculated.amount.error.outOfRange"))
+          )
+        }
+      )(PremiumCalculated.apply)(PremiumCalculated.unapply)
+    )
+}
---
+++ app/viewmodels/checkAnswers/ukandforeignproperty/UkAndForeignCalculatedForeignPremiumGrantLeaseTaxableSummary.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.checkAnswers.ukandforeignproperty
+
+import controllers.routes
+import models.{CheckMode, PremiumCalculated, UserAnswers}
+import pages.ukandforeignproperty.UkAndForeignCalculatedForeignPremiumGrantLeaseTaxablePage
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import viewmodels.checkAnswers.FormatUtils.{keyCssClass, valueCssClass}
+import viewmodels.govuk.summarylist._
+import viewmodels.implicits._
+
+object UkAndForeignCalculatedForeignPremiumGrantLeaseTaxableSummary  {
+
+  def row(taxYear: Int, countryCode: String, answers: UserAnswers)(implicit messages: Messages): Option[SummaryListRow] =
+  answers.get(UkAndForeignCalculatedForeignPremiumGrantLeaseTaxablePage(countryCode)).flatMap {
+    case PremiumCalculated(true, _) =>
+      Some(SummaryListRowViewModel(
+        key = KeyViewModel("ukAndForeignCalculatedForeignPremiumGrantLeaseTaxable.checkYourAnswersLabel").withCssClass(keyCssClass),
+        value = ValueViewModel("site.yes").withCssClass(valueCssClass),
+        actions = Seq(
+          ActionItemViewModel("site.change",
+            controllers.ukandforeignproperty.routes.UkAndForeignCalculatedForeignPremiumGrantLeaseTaxableController.onPageLoad(taxYear, countryCode, CheckMode).url)
+            .withVisuallyHiddenText(messages("ukAndForeignCalculatedForeignPremiumGrantLeaseTaxable.change.hidden"))
+        )))
+    case PremiumCalculated(false, _) =>
+      Some(SummaryListRowViewModel(
+        key = KeyViewModel("ukAndForeignCalculatedForeignPremiumGrantLeaseTaxable.checkYourAnswersLabel").withCssClass(keyCssClass),
+        value = ValueViewModel("site.no").withCssClass(valueCssClass),
+        actions = Seq(
+          ActionItemViewModel("site.change",
+            controllers.ukandforeignproperty.routes.UkAndForeignCalculatedForeignPremiumGrantLeaseTaxableController.onPageLoad(taxYear, countryCode, CheckMode).url)
+            .withVisuallyHiddenText(messages("ukAndForeignCalculatedForeignPremiumGrantLeaseTaxable.change.hidden"))
+        )
+      ))
+    case _ => Option.empty[SummaryListRow]
+  }
+}
