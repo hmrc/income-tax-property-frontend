75593d8e360c7db6ffde20fec51f5e548ac0e662
iyadi <7164084+iyadi@users.noreply.github.com>
2025-01-27 10:04:42 +0000

    SASS-10924: UK & Foreign Property Mini Journey - Create 'Other income from foreign property' page

---
+++ test/controllers/ukandforeignproperty/income/UkAndForeignPropertyForeignOtherIncomeFromPropertyControllerSpec.scala
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.ukandforeignproperty.income
+
+import base.SpecBase
+import controllers.routes
+import forms.ukandforeignproperty.income.UkAndForeignPropertyForeignOtherIncomeFromPropertyFormProvider
+import models.{NormalMode, UserAnswers}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.ukandforeignproperty.income.UkAndForeignPropertyForeignOtherIncomeFromPropertyPage
+import play.api.Application
+import play.api.data.Form
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import repositories.SessionRepository
+import views.html.ukandforeignproperty.income.UkAndForeignPropertyForeignOtherIncomeFromPropertyView
+
+import scala.concurrent.Future
+
+class UkAndForeignPropertyForeignOtherIncomeFromPropertyControllerSpec extends SpecBase with MockitoSugar {
+
+  def onwardRoute: Call = Call("GET", "/foo")
+
+  val individualOrAgent: String = "individual"
+  val taxYear: Int = 2024
+  val countryCode: String = "AUS"
+  val otherIncomeFromProperty: BigDecimal = BigDecimal(12345)
+  val formProvider = new UkAndForeignPropertyForeignOtherIncomeFromPropertyFormProvider()
+  val form: Form[BigDecimal] = formProvider(individualOrAgent)
+
+  lazy val foreignOtherIncomeFromPropertyRoute =
+    controllers.ukandforeignproperty.income.routes.UkAndForeignPropertyForeignOtherIncomeFromPropertyController.onPageLoad(taxYear, countryCode, NormalMode).url
+
+  "UkAndForeignPropertyForeignOtherIncomeFromProperty Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false).build()
+
+      running(application) {
+        val request = FakeRequest(GET, foreignOtherIncomeFromPropertyRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[UkAndForeignPropertyForeignOtherIncomeFromPropertyView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, taxYear, countryCode, NormalMode, individualOrAgent)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = UserAnswers(userAnswersId).set(UkAndForeignPropertyForeignOtherIncomeFromPropertyPage(countryCode), otherIncomeFromProperty).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers), isAgent = false).build()
+
+      running(application) {
+        val request = FakeRequest(GET, foreignOtherIncomeFromPropertyRoute)
+
+        val view = application.injector.instanceOf[UkAndForeignPropertyForeignOtherIncomeFromPropertyView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(
+          form.fill(otherIncomeFromProperty),
+          taxYear,
+          countryCode,
+          NormalMode,
+          individualOrAgent)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application: Application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, foreignOtherIncomeFromPropertyRoute)
+            .withFormUrlEncodedBody(("otherPropertyIncome", otherIncomeFromProperty.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        val redirect = redirectLocation(result)
+        redirect.value mustEqual controllers.routes.IndexController.onPageLoad.url //TODO update when Navigation is integrated
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers), isAgent = false).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, foreignOtherIncomeFromPropertyRoute)
+            .withFormUrlEncodedBody(("otherPropertyIncome", "invalid value"))
+
+        val boundForm = form.bind(Map("otherPropertyIncome" -> "invalid value"))
+
+        val view = application.injector.instanceOf[UkAndForeignPropertyForeignOtherIncomeFromPropertyView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, taxYear, countryCode, NormalMode, individualOrAgent)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None, true).build()
+
+      running(application) {
+        val request = FakeRequest(GET, foreignOtherIncomeFromPropertyRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None, isAgent = true).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, foreignOtherIncomeFromPropertyRoute)
+            .withFormUrlEncodedBody(("otherPropertyIncome", otherIncomeFromProperty.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
---
+++ test/forms/ukandforeignproperty/income/UkAndForeignPropertyForeignOtherIncomeFromPropertyFormProviderSpec.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.ukandforeignproperty.income
+
+import forms.behaviours.CurrencyFieldBehaviours
+import org.scalatest.prop.TableFor1
+import play.api.data.FormError
+
+class UkAndForeignPropertyForeignOtherIncomeFromPropertyFormProviderSpec extends CurrencyFieldBehaviours {
+
+  val scenarios: TableFor1[String] = Table[String]( "individual", "agent")
+  val fieldName = "otherPropertyIncome"
+
+  val minimum = 0
+  val maximum = 100000000
+  val formProvider = new UkAndForeignPropertyForeignOtherIncomeFromPropertyFormProvider()
+
+  forAll(scenarios) { (individualOrAgent: String) =>
+    val form = formProvider(individualOrAgent)
+    val requiredKey = s"ukAndForeignProperty.otherIncomeFromForeignProperty.error.required.$individualOrAgent"
+    val outOutRangeErrorKey = s"ukAndForeignProperty.otherIncomeFromForeignProperty.error.outOfRange.$individualOrAgent"
+    val nonNumericErrorKey = s"ukAndForeignProperty.otherIncomeFromForeignProperty.error.nonNumeric.$individualOrAgent"
+    val twoDecimalErrorKey = s"ukAndForeignProperty.otherIncomeFromForeignProperty.error.twoDecimalPlaces.$individualOrAgent"
+
+    s".foreignPropertyRentalIncome for an $individualOrAgent" - {
+
+      val validDataGenerator = intsInRangeWithCommas(minimum, maximum)
+
+      behave like fieldThatBindsValidData(
+        form,
+        fieldName,
+        validDataGenerator
+      )
+
+      behave like currencyField(
+        form,
+        fieldName,
+        nonNumericError = FormError(fieldName, nonNumericErrorKey),
+        twoDecimalPlacesError = FormError(fieldName, twoDecimalErrorKey)
+      )
+
+      behave like currencyFieldWithRange(
+        form,
+        fieldName,
+        minimum = minimum,
+        maximum = maximum,
+        expectedError = FormError(fieldName, outOutRangeErrorKey, Seq(minimum, maximum))
+      )
+
+      behave like mandatoryField(
+        form,
+        fieldName,
+        requiredError = FormError(fieldName, requiredKey)
+      )
+    }
+  }
+}
