caae8204c8fa4f31c21b28c5441e818b97090a1b
tbg2003 <72973046+tbg2003@users.noreply.github.com>
2025-01-27 11:34:57 +0000

    SASS-10906:  UK & Foreign Property Mini Journey - Create 'Premiums for the grant of a lease' (UK) page

---
+++ app/pages/ukandforeignproperty/UKPremiumsGrantLeasePage.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pages.ukandforeignproperty
+
+import models.TotalIncomeUtils.isTotalIncomeUnder85K
+import models.ukAndForeign.UKPremiumsGrantLease
+import models.{UKAndForeignProperty, UserAnswers}
+import pages.PageConstants.incomePath
+import pages.QuestionPage
+import pages.propertyrentals.expenses.ConsolidatedExpensesPage
+import play.api.libs.json.JsPath
+
+import scala.util.Try
+
+object UKPremiumsGrantLeasePage extends QuestionPage[UKPremiumsGrantLease] {
+
+  override def path: JsPath = JsPath \ incomePath(UKAndForeignProperty) \ toString
+
+  override def toString: String = "premiumsGrantLease"
+
+  def calculateTaxableAmount(premiumAmount: BigDecimal, periods: Int): BigDecimal =
+    (premiumAmount * (BigDecimal(50 - minusOne(periods)) / 50)).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+
+  def minusOne(periods: Int): Int = periods - 1
+
+  override def cleanup(value: Option[UKPremiumsGrantLease], userAnswers: UserAnswers): Try[UserAnswers] =
+    if (isTotalIncomeUnder85K(userAnswers, UKAndForeignProperty)) {
+      super.cleanup(value, userAnswers)
+    } else if (
+      userAnswers
+        .get(ConsolidatedExpensesPage(UKAndForeignProperty))
+        .fold(false)(data => data.consolidatedExpensesYesOrNo)
+    ) {
+      userAnswers.remove(ConsolidatedExpensesPage(UKAndForeignProperty))
+    } else {
+      super.cleanup(value, userAnswers)
+    }
+
+}
---
+++ test/forms/ukandforeignproperty/UKPremiumsGrantLeaseFormProviderSpec.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.ukandforeignproperty
+
+import forms.behaviours.CurrencyFieldBehaviours
+import play.api.data.FormError
+
+class UKPremiumsGrantLeaseFormProviderSpec extends CurrencyFieldBehaviours {
+
+  val form = new UKPremiumsGrantLeaseFormProvider()("individual")
+
+  ".premiumsGrantLeaseYesNo" - {
+
+    val fieldName = "premiumsGrantLeaseReceived"
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, "ukPremiumsGrantLease.error.required.individual")
+    )
+  }
+
+  ".premiumsGrantLease for agent" - {
+
+
+    val fieldName = "premiumsGrantLeaseAmount"
+
+    val minimum = 0
+    val maximum = 100000000
+
+    val validDataGenerator = intsInRangeWithCommas(minimum, maximum)
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      validDataGenerator
+    )
+
+    behave like currencyField(
+      form,
+      fieldName,
+      nonNumericError = FormError(fieldName, "ukPremiumsGrantLease.error.amount.nonNumeric.individual"),
+      twoDecimalPlacesError = FormError(fieldName, "ukPremiumsGrantLease.error.amount.twoDecimalPlaces"),
+      defaultFields = ("premiumsGrantLeaseReceived", "false")
+    )
+
+    behave like currencyFieldWithRange(
+      form,
+      fieldName,
+      minimum = minimum,
+      maximum = maximum,
+      expectedError = FormError(fieldName, "ukPremiumsGrantLease.error.outOfRange", Seq(minimum, maximum)),
+      defaultFields = ("premiumsGrantLeaseReceived", "false")
+    )
+  }
+}
