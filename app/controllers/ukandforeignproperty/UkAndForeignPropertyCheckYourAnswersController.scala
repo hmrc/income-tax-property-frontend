8418be22e25b5376bae4533297ecc48fbca170bc
tbg2003 <72973046+tbg2003@users.noreply.github.com>
2025-01-28 16:29:44 +0000

    wip - skeleton view and controller

--- app/controllers/ukandforeignproperty/UkAndForeignPropertyCheckYourAnswersController.scala
+++ app/controllers/ukandforeignproperty/UkAndForeignPropertyCheckYourAnswersController.scala
@@ -20,18 +20,19 @@ import controllers.actions._
 import controllers.exceptions.{NotFoundException, SaveJourneyAnswersFailed}
 import models.requests.DataRequest
 import models.ukAndForeign.UkAndForeignAbout
-import models.{JourneyContext, JourneyPath}
-import pages.ukandforeignproperty.UkForeignPropertyAboutPage
+import models.{JourneyContext, JourneyPath, UKAndForeignProperty, UkAndForeignPropertyClaimExpensesOrRelief}
+import pages.ukandforeignproperty.{UkAndForeignPropertyClaimPropertyIncomeAllowanceOrExpensesPage, UkAndForeignPropertyIncomeAllowanceClaimPage, UkForeignPropertyAboutPage}
 import play.api.Logging
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
 import service.PropertySubmissionService
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryList
 import uk.gov.hmrc.http.HeaderCarrier
 
 import scala.concurrent.Future
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import viewmodels.checkAnswers.ReportIncomeSummary
-import viewmodels.checkAnswers.ukandforeignproperty.TotalPropertyIncomeSummary
+import viewmodels.checkAnswers.{ReportIncomeSummary, UkAndForeignPropertyRentalTypeUkSummary}
+import viewmodels.checkAnswers.ukandforeignproperty._
 import viewmodels.govuk.all.SummaryListViewModel
 import views.html.ukandforeignproperty.UkAndForeignPropertyCheckYourAnswersView
 
@@ -57,33 +58,97 @@ class UkAndForeignPropertyCheckYourAnswersController @Inject() (
           ReportIncomeSummary.row(taxYear = taxYear, request.user.isAgentMessageKey, answers = request.userAnswers)
         ).flatten
       )
-      Ok(view(list, taxYear))
+
+      val ukAndForeignPropertyList =
+        SummaryListViewModel(
+          rows = Seq(
+            TotalPropertyIncomeSummary.row(taxYear = taxYear, answers = request.userAnswers),
+            ReportIncomeSummary.row(taxYear = taxYear, request.user.isAgentMessageKey, answers = request.userAnswers),
+            UkAndForeignPropertyRentalTypeUkSummary.row(taxYear = taxYear, answers = request.userAnswers)
+            // Foreign Countries Summary row
+            // UkAndForeignPropertyClaimExpensesOrReliefSummary
+//            PropertyIncomeAllowanceClaimSummary.row(taxYear = taxYear, answers = request.userAnswers)
+          ).flatten
+        )
+
+      val ukPropertyList: Option[SummaryList] = {
+        val isPIA =
+          request.userAnswers
+            .get(UkAndForeignPropertyClaimPropertyIncomeAllowanceOrExpensesPage)
+            .exists(_.claimPropertyIncomeAllowanceOrExpensesYesNo)
+
+        if (isPIA) {
+          Some(
+            SummaryListViewModel(
+              rows = Seq(
+                // Non uk landlord
+                // deducting tax from non uk landlord
+                // how much income from uk rentals
+                // uk balancing charge
+                // uk premiums for grant of lease
+                // have you calc the premium
+                // how much did you recieve
+                // how many complete 12 month periods
+                // premimums for grant of lease
+                // uk reveresed preiums recieved
+                // uk property other income
+              ).flatten
+            )
+          )
+        } else {
+          None
+        }
+      }
+
+      val foreignPropertyList =
+        SummaryListViewModel(
+          rows = Seq(
+            // how much income from foreign rentals
+            // foreign balancing charge
+            // foreign premiums for grant of lease
+            // have you calc the premium
+            // how much did you recieve
+            // how many complete 12 month periods
+            // premimums for grant of lease
+            // foreign reveresed preiums recieved
+            // foreign property other income
+            // your property income allowance claim
+          ).flatten
+        )
+
+      Ok(view(list, None, Some(list), taxYear))
     }
 
   def onSubmit(taxYear: Int): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
-
       request.userAnswers
         .get(UkForeignPropertyAboutPage)
         .map { ukAndForeignAbout =>
           savePropertyAbout(taxYear, request, ukAndForeignAbout)
         }
         .getOrElse {
-          logger.error(s"Uk and foreign property about section is not present in userAnswers for userId: ${request.userId}")
+          logger.error(
+            s"Uk and foreign property about section is not present in userAnswers for userId: ${request.userId}"
+          )
           Future.failed(NotFoundException("Uk and foreign property about section is not present in userAnswers"))
         }
   }
 
-  private def savePropertyAbout(taxYear: Int, request: DataRequest[AnyContent], ukAndForeignAbout: UkAndForeignAbout)(implicit
-                                                                                                                  hc: HeaderCarrier
+  private def savePropertyAbout(taxYear: Int, request: DataRequest[AnyContent], ukAndForeignAbout: UkAndForeignAbout)(
+    implicit hc: HeaderCarrier
   ): Future[Result] = {
 
-    val context = JourneyContext(taxYear, request.user.mtditid, request.user.nino, JourneyPath.UkAndForeignPropertyAbout)
+    val context =
+      JourneyContext(taxYear, request.user.mtditid, request.user.nino, JourneyPath.UkAndForeignPropertyAbout)
 
     propertySubmissionService.saveJourneyAnswers(context, ukAndForeignAbout).flatMap {
       case Right(_) =>
-      //TODO redirect to a 'Have you finished this section' page / completion controller
-      Future.successful(Redirect(controllers.ukandforeignproperty.routes.UkAndForeignPropertyDetailsController.onPageLoad(taxYear: Int)))
+        // TODO redirect to a 'Have you finished this section' page / completion controller
+        Future.successful(
+          Redirect(
+            controllers.ukandforeignproperty.routes.UkAndForeignPropertyDetailsController.onPageLoad(taxYear: Int)
+          )
+        )
       case Left(error) =>
         logger.error(s"Failed to save uk and foreign property about section: ${error.toString}")
         Future.failed(SaveJourneyAnswersFailed("Failed to save uk and foreign property about section"))
