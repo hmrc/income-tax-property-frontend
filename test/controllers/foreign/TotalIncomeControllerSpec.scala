344d5b30e9d271b2b61791e9528a194b359584db
Stuart Proctor <112641125+StuartJamesProctor@users.noreply.github.com>
2025-02-05 09:29:44 +0000

    SASS-10966 Fix so user can start with foreign property (#585)

--- test/controllers/foreign/TotalIncomeControllerSpec.scala
+++ test/controllers/foreign/TotalIncomeControllerSpec.scala
@@ -135,20 +135,6 @@ class TotalIncomeControllerSpec extends SpecBase with MockitoSugar {
         }
       }
 
-      "must redirect to Journey Recovery for a GET if no existing data is found" in {
-
-        val application = applicationBuilder(userAnswers = None, isAgent = isAgent).build()
-
-        running(application) {
-          val request = FakeRequest(GET, totalIncomeRoute)
-
-          val result = route(application, request).value
-
-          status(result) mustEqual SEE_OTHER
-          redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
-        }
-      }
-
       "redirect to Journey Recovery for a POST if no existing data is found" in {
 
         val application = applicationBuilder(userAnswers = None, isAgent = isAgent).build()
