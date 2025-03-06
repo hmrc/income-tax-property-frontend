34f209038e5461e91650c02e52033fc13911aa9c
stephen-oppong-beduh <56881842+stephen-oppong-beduh@users.noreply.github.com>
2025-03-06 15:30:46 +0000

    SASS-11260 - PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.5.0"
-  private val hmrcMongoVersion = "2.2.0"
+  private val bootstrapVersion = "9.10.0"
+  private val hmrcMongoVersion = "2.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
