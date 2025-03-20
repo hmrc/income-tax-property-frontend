d273befebf87145302c20beeb32e890809a39933
RGlossop <75027918+RGlossop@users.noreply.github.com>
2025-03-20 11:09:41 +0000

    SAS-10552 AboutSectionComplete updated

--- app/forms/ukrentaroom/AboutSectionCompleteFormProvider.scala
+++ app/forms/ukrentaroom/AboutSectionCompleteFormProvider.scala
@@ -25,6 +25,6 @@ class AboutSectionCompleteFormProvider @Inject() extends Mappings {
 
   def apply(): Form[Boolean] =
     Form(
-      "rentARoomIsSectionCompleteYesOrNo" -> boolean("haveYouFinishedThisSection.error.required")
+      "isRentARoomIsSectionComplete" -> boolean("haveYouFinishedThisSection.error.required")
     )
 }
