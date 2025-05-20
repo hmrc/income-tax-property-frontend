f40a3cdd96aa47dc28d7bcb612bcc29b84c69834
Matthew Ward <96430760+matthewwardcap@users.noreply.github.com>
2025-05-20 15:45:26 +0100

    SASS-11589: Add connector to income-tax-session-data µService (#673)

---
+++ app/connectors/SessionDataConnector.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import config.FrontendAppConfig
+import connectors.response.SessionDataHttpResponse.{SessionDataResponse, SessionDataResponseReads}
+import play.api.Logging
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, StringContextOps}
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class SessionDataConnector @Inject()(config: FrontendAppConfig,
+                                     httpClient: HttpClientV2)(implicit ec: ExecutionContext) extends Logging {
+
+  def getSessionData(implicit hc: HeaderCarrier): Future[SessionDataResponse] =
+    httpClient
+      .get(url"${config.vcSessionServiceBaseUrl}/income-tax-session-data")
+      .execute[SessionDataResponse]
+
+}
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,29 +2,29 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.10.0"
-  private val hmrcMongoVersion = "2.5.0"
+  private val bootstrapVersion = "9.11.0"
+  private val hmrcMongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"            % "10.12.0",
-    "uk.gov.hmrc"          %% "play-conditional-form-mapping-play-30" % "2.0.0",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"            % "12.1.0",
+    "uk.gov.hmrc"          %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
-    "com.github.tototoshi" %% "scala-csv"                             % "1.3.10"
+    "com.github.tototoshi" %% "scala-csv"                             % "2.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatest"       %% "scalatest"               % "3.2.18",
+    "org.scalatest"       %% "scalatest"               % "3.2.19",
     "org.scalatestplus"   %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus"   %% "mockito-3-4"             % "3.2.10.0",
-    "org.mockito"         %% "mockito-scala"           % "1.17.31",
+    "org.mockito"         %% "mockito-scala"           % "1.17.37",
     "org.scalamock"       %% "scalamock"               % "5.2.0",
-    "org.scalacheck"      %% "scalacheck"              % "1.18.0",
+    "org.scalacheck"      %% "scalacheck"              % "1.18.1",
     "org.pegdown"          % "pegdown"                 % "1.6.0",
-    "org.jsoup"            % "jsoup"                   % "1.17.2",
+    "org.jsoup"            % "jsoup"                   % "1.20.1",
     "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8"
   ).map(_ % Test)
 
