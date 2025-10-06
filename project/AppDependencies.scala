d7b02703e22891d91f083384a9db354089999d7e
tbg2003 <72973046+tbg2003@users.noreply.github.com>
2025-10-06 15:48:34 +0100

    MIPR-1976: TEAM B - Upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.1.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val bootstrapVersion = "10.2.0"
+  private val hmrcMongoVersion = "2.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
